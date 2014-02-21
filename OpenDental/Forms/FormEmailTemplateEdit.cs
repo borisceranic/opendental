3cc7b2fdebbe3d67dcdeafae5b4cae0e97299b0f
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-02-21 18:55:38 +0000

    Recalls, email subject only allow a 200 character maximum. Text boxes in other places had their maximum length set to 200 characters to match this change.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.1@10702 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormEmailTemplateEdit.cs
+++ OpenDental/Forms/FormEmailTemplateEdit.cs
@@ -65,65 +65,68 @@ namespace OpenDental{
 			// 
 			// butCancel
 			// 
-			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butCancel.Autosize = true;
 			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butCancel.CornerRadius = 4F;
 			this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.butCancel.Location = new System.Drawing.Point(755,505);
+			this.butCancel.Location = new System.Drawing.Point(755, 505);
 			this.butCancel.Name = "butCancel";
-			this.butCancel.Size = new System.Drawing.Size(75,25);
+			this.butCancel.Size = new System.Drawing.Size(75, 25);
 			this.butCancel.TabIndex = 3;
 			this.butCancel.Text = "&Cancel";
 			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
 			// 
 			// butOK
 			// 
-			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butOK.Autosize = true;
 			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butOK.CornerRadius = 4F;
-			this.butOK.Location = new System.Drawing.Point(755,472);
+			this.butOK.Location = new System.Drawing.Point(755, 472);
 			this.butOK.Name = "butOK";
-			this.butOK.Size = new System.Drawing.Size(75,25);
+			this.butOK.Size = new System.Drawing.Size(75, 25);
 			this.butOK.TabIndex = 2;
 			this.butOK.Text = "&OK";
 			this.butOK.Click += new System.EventHandler(this.butOK_Click);
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(7,12);
+			this.label2.Location = new System.Drawing.Point(7, 12);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(88,14);
+			this.label2.Size = new System.Drawing.Size(88, 14);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Subject:";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// textSubject
 			// 
-			this.textSubject.Location = new System.Drawing.Point(99,7);
+			this.textSubject.Location = new System.Drawing.Point(99, 7);
+			this.textSubject.MaxLength = 200;
 			this.textSubject.Name = "textSubject";
-			this.textSubject.Size = new System.Drawing.Size(741,20);
+			this.textSubject.Size = new System.Drawing.Size(741, 20);
 			this.textSubject.TabIndex = 0;
 			// 
 			// textBodyText
 			// 
-			this.textBodyText.Location = new System.Drawing.Point(99,32);
-			this.textBodyText.Multiline = true;
+			this.textBodyText.AcceptsTab = true;
+			this.textBodyText.DetectUrls = false;
+			this.textBodyText.Location = new System.Drawing.Point(99, 32);
 			this.textBodyText.Name = "textBodyText";
 			this.textBodyText.QuickPasteType = OpenDentBusiness.QuickPasteType.Email;
 			this.textBodyText.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-			this.textBodyText.Size = new System.Drawing.Size(741,426);
+			this.textBodyText.Size = new System.Drawing.Size(741, 426);
 			this.textBodyText.TabIndex = 4;
+			this.textBodyText.Text = "";
 			// 
 			// FormEmailTemplateEdit
 			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
-			this.ClientSize = new System.Drawing.Size(844,544);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(844, 544);
 			this.Controls.Add(this.textBodyText);
 			this.Controls.Add(this.textSubject);
 			this.Controls.Add(this.label2);
--- OpenDental/Forms/FormRecallMessageEdit.cs
+++ OpenDental/Forms/FormRecallMessageEdit.cs
@@ -1,18 +1,16 @@
+using OpenDentBusiness;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace OpenDental {
 	public partial class FormRecallMessageEdit:Form {
 		public string MessageVal;
+		private PrefName _prefName;
 
-		public FormRecallMessageEdit() {
+		public FormRecallMessageEdit(PrefName prefName) {
 			InitializeComponent();
 			Lan.F(this);
+			_prefName=prefName;
 		}
 
 		private void FormRecallMessageEdit_Load(object sender,EventArgs e) {
@@ -20,6 +18,18 @@ namespace OpenDental {
 		}
 
 		private void butOK_Click(object sender,EventArgs e) {
+			//We need to limit email subjects to 200 characters otherwise errors can happen in other places of the software and it's hard to track.
+			//E.g. sending emails from the Recall List window and all recalls of type email will simply skip with no explanation.
+			if(_prefName==PrefName.BillingEmailSubject
+				|| _prefName==PrefName.ConfirmEmailSubject
+				|| _prefName==PrefName.RecallEmailSubject
+				|| _prefName==PrefName.RecallEmailSubject2
+				|| _prefName==PrefName.RecallEmailSubject3) {
+				if(textMain.Text.Length>200) {
+					MsgBox.Show(this,"Email subjects cannot be longer than 200 characters.");
+					return;
+				}
+			}
 			MessageVal=textMain.Text;
 			DialogResult=DialogResult.OK;
 		}
