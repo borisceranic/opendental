abc32c8d82071988147abb8262929e17b13ede17
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-03-04 18:38:20 +0000

    Payment, some error fixing was being skipped in certain situations.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.1@10744 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormCreditRecurringCharges.designer.cs
+++ OpenDental/Forms/FormCreditRecurringCharges.designer.cs
@@ -23,6 +23,7 @@ namespace OpenDental{
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCreditRecurringCharges));
 			this.groupCounts = new System.Windows.Forms.GroupBox();
 			this.labelFailed = new System.Windows.Forms.Label();
 			this.labelCharged = new System.Windows.Forms.Label();
@@ -45,90 +46,90 @@ namespace OpenDental{
 			this.groupCounts.Controls.Add(this.labelCharged);
 			this.groupCounts.Controls.Add(this.labelSelected);
 			this.groupCounts.Controls.Add(this.labelTotal);
-			this.groupCounts.Location = new System.Drawing.Point(617,196);
+			this.groupCounts.Location = new System.Drawing.Point(617, 196);
 			this.groupCounts.Name = "groupCounts";
-			this.groupCounts.Size = new System.Drawing.Size(96,103);
+			this.groupCounts.Size = new System.Drawing.Size(96, 103);
 			this.groupCounts.TabIndex = 34;
 			this.groupCounts.TabStop = false;
 			this.groupCounts.Text = "Counts";
 			// 
 			// labelFailed
 			// 
-			this.labelFailed.Location = new System.Drawing.Point(4,76);
+			this.labelFailed.Location = new System.Drawing.Point(4, 76);
 			this.labelFailed.Name = "labelFailed";
-			this.labelFailed.Size = new System.Drawing.Size(89,16);
+			this.labelFailed.Size = new System.Drawing.Size(89, 16);
 			this.labelFailed.TabIndex = 32;
 			this.labelFailed.Text = "Failed=0";
 			this.labelFailed.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// labelCharged
 			// 
-			this.labelCharged.Location = new System.Drawing.Point(4,57);
+			this.labelCharged.Location = new System.Drawing.Point(4, 57);
 			this.labelCharged.Name = "labelCharged";
-			this.labelCharged.Size = new System.Drawing.Size(89,16);
+			this.labelCharged.Size = new System.Drawing.Size(89, 16);
 			this.labelCharged.TabIndex = 31;
 			this.labelCharged.Text = "Charged=0";
 			this.labelCharged.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// labelSelected
 			// 
-			this.labelSelected.Location = new System.Drawing.Point(4,38);
+			this.labelSelected.Location = new System.Drawing.Point(4, 38);
 			this.labelSelected.Name = "labelSelected";
-			this.labelSelected.Size = new System.Drawing.Size(89,16);
+			this.labelSelected.Size = new System.Drawing.Size(89, 16);
 			this.labelSelected.TabIndex = 30;
 			this.labelSelected.Text = "Selected=0";
 			this.labelSelected.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// labelTotal
 			// 
-			this.labelTotal.Location = new System.Drawing.Point(4,19);
+			this.labelTotal.Location = new System.Drawing.Point(4, 19);
 			this.labelTotal.Name = "labelTotal";
-			this.labelTotal.Size = new System.Drawing.Size(89,16);
+			this.labelTotal.Size = new System.Drawing.Size(89, 16);
 			this.labelTotal.TabIndex = 29;
 			this.labelTotal.Text = "Total=0";
 			this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// butNone
 			// 
-			this.butNone.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butNone.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butNone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.butNone.Autosize = true;
 			this.butNone.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butNone.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butNone.CornerRadius = 4F;
-			this.butNone.Location = new System.Drawing.Point(102,498);
+			this.butNone.Location = new System.Drawing.Point(102, 498);
 			this.butNone.Name = "butNone";
-			this.butNone.Size = new System.Drawing.Size(75,24);
+			this.butNone.Size = new System.Drawing.Size(75, 24);
 			this.butNone.TabIndex = 42;
 			this.butNone.Text = "&None";
 			this.butNone.Click += new System.EventHandler(this.butNone_Click);
 			// 
 			// butAll
 			// 
-			this.butAll.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butAll.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.butAll.Autosize = true;
 			this.butAll.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butAll.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butAll.CornerRadius = 4F;
-			this.butAll.Location = new System.Drawing.Point(12,498);
+			this.butAll.Location = new System.Drawing.Point(12, 498);
 			this.butAll.Name = "butAll";
-			this.butAll.Size = new System.Drawing.Size(75,24);
+			this.butAll.Size = new System.Drawing.Size(75, 24);
 			this.butAll.TabIndex = 41;
 			this.butAll.Text = "&All";
 			this.butAll.Click += new System.EventHandler(this.butAll_Click);
 			// 
 			// butRefresh
 			// 
-			this.butRefresh.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butRefresh.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.butRefresh.Autosize = true;
 			this.butRefresh.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butRefresh.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butRefresh.CornerRadius = 4F;
-			this.butRefresh.Location = new System.Drawing.Point(627,12);
+			this.butRefresh.Location = new System.Drawing.Point(627, 12);
 			this.butRefresh.Name = "butRefresh";
-			this.butRefresh.Size = new System.Drawing.Size(75,24);
+			this.butRefresh.Size = new System.Drawing.Size(75, 24);
 			this.butRefresh.TabIndex = 40;
 			this.butRefresh.Text = "Refresh";
 			this.butRefresh.Click += new System.EventHandler(this.butRefresh_Click);
@@ -139,11 +140,11 @@ namespace OpenDental{
             | System.Windows.Forms.AnchorStyles.Left)));
 			this.gridMain.AutoScroll = true;
 			this.gridMain.HScrollVisible = false;
-			this.gridMain.Location = new System.Drawing.Point(12,12);
+			this.gridMain.Location = new System.Drawing.Point(12, 12);
 			this.gridMain.Name = "gridMain";
 			this.gridMain.ScrollValue = 0;
 			this.gridMain.SelectionMode = OpenDental.UI.GridSelectionMode.MultiExtended;
-			this.gridMain.Size = new System.Drawing.Size(592,474);
+			this.gridMain.Size = new System.Drawing.Size(592, 474);
 			this.gridMain.TabIndex = 29;
 			this.gridMain.Title = "Recurring Charges";
 			this.gridMain.TranslationName = "TableRecurring";
@@ -152,37 +153,37 @@ namespace OpenDental{
 			// 
 			// butSend
 			// 
-			this.butSend.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butSend.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butSend.Autosize = true;
 			this.butSend.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butSend.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butSend.CornerRadius = 4F;
-			this.butSend.Location = new System.Drawing.Point(627,462);
+			this.butSend.Location = new System.Drawing.Point(627, 462);
 			this.butSend.Name = "butSend";
-			this.butSend.Size = new System.Drawing.Size(75,24);
+			this.butSend.Size = new System.Drawing.Size(75, 24);
 			this.butSend.TabIndex = 3;
 			this.butSend.Text = "&Send";
 			this.butSend.Click += new System.EventHandler(this.butSend_Click);
 			// 
 			// butCancel
 			// 
-			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butCancel.Autosize = true;
 			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butCancel.CornerRadius = 4F;
-			this.butCancel.Location = new System.Drawing.Point(627,498);
+			this.butCancel.Location = new System.Drawing.Point(627, 498);
 			this.butCancel.Name = "butCancel";
-			this.butCancel.Size = new System.Drawing.Size(75,24);
+			this.butCancel.Size = new System.Drawing.Size(75, 24);
 			this.butCancel.TabIndex = 2;
 			this.butCancel.Text = "Close";
 			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
 			// 
 			// butPrintList
 			// 
-			this.butPrintList.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butPrintList.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butPrintList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.butPrintList.Autosize = true;
 			this.butPrintList.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
@@ -190,9 +191,9 @@ namespace OpenDental{
 			this.butPrintList.CornerRadius = 4F;
 			this.butPrintList.Image = global::OpenDental.Properties.Resources.butPrint;
 			this.butPrintList.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butPrintList.Location = new System.Drawing.Point(318,498);
+			this.butPrintList.Location = new System.Drawing.Point(318, 498);
 			this.butPrintList.Name = "butPrintList";
-			this.butPrintList.Size = new System.Drawing.Size(88,24);
+			this.butPrintList.Size = new System.Drawing.Size(88, 24);
 			this.butPrintList.TabIndex = 43;
 			this.butPrintList.Text = "Print List";
 			this.butPrintList.Click += new System.EventHandler(this.butPrintList_Click);
@@ -200,7 +201,7 @@ namespace OpenDental{
 			// FormCreditRecurringCharges
 			// 
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.ClientSize = new System.Drawing.Size(725,534);
+			this.ClientSize = new System.Drawing.Size(725, 534);
 			this.Controls.Add(this.butPrintList);
 			this.Controls.Add(this.butNone);
 			this.Controls.Add(this.butAll);
@@ -209,6 +210,7 @@ namespace OpenDental{
 			this.Controls.Add(this.gridMain);
 			this.Controls.Add(this.butSend);
 			this.Controls.Add(this.butCancel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "FormCreditRecurringCharges";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Credit Card Recurring Charges";
