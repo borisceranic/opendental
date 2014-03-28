55de650410f48e6a6f825863ea54cd46b583082e
sam <sam@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-03-28 23:38:11 +0000

    ServiceManager project is now more generic and will install more than just HL7 service. Specifically, the changes were made so it could manage the OpenDentCustListener service also.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@10827 098d0599-7f2b-0410-95a5-ad30ac85393a

--- ServiceManager/FormServiceManage.Designer.cs
+++ ServiceManager/FormServiceManage.Designer.cs
@@ -39,9 +39,9 @@
 			// 
 			// butRefresh
 			// 
-			this.butRefresh.Location = new System.Drawing.Point(377,80);
+			this.butRefresh.Location = new System.Drawing.Point(377, 80);
 			this.butRefresh.Name = "butRefresh";
-			this.butRefresh.Size = new System.Drawing.Size(75,23);
+			this.butRefresh.Size = new System.Drawing.Size(75, 23);
 			this.butRefresh.TabIndex = 4;
 			this.butRefresh.Text = "Refresh";
 			this.butRefresh.UseVisualStyleBackColor = true;
@@ -49,9 +49,9 @@
 			// 
 			// butStop
 			// 
-			this.butStop.Location = new System.Drawing.Point(359,113);
+			this.butStop.Location = new System.Drawing.Point(359, 113);
 			this.butStop.Name = "butStop";
-			this.butStop.Size = new System.Drawing.Size(75,23);
+			this.butStop.Size = new System.Drawing.Size(75, 23);
 			this.butStop.TabIndex = 3;
 			this.butStop.Text = "Stop";
 			this.butStop.UseVisualStyleBackColor = true;
@@ -59,9 +59,9 @@
 			// 
 			// butStart
 			// 
-			this.butStart.Location = new System.Drawing.Point(278,113);
+			this.butStart.Location = new System.Drawing.Point(278, 113);
 			this.butStart.Name = "butStart";
-			this.butStart.Size = new System.Drawing.Size(75,23);
+			this.butStart.Size = new System.Drawing.Size(75, 23);
 			this.butStart.TabIndex = 2;
 			this.butStart.Text = "Start";
 			this.butStart.UseVisualStyleBackColor = true;
@@ -69,9 +69,9 @@
 			// 
 			// butUninstall
 			// 
-			this.butUninstall.Location = new System.Drawing.Point(197,113);
+			this.butUninstall.Location = new System.Drawing.Point(197, 113);
 			this.butUninstall.Name = "butUninstall";
-			this.butUninstall.Size = new System.Drawing.Size(75,23);
+			this.butUninstall.Size = new System.Drawing.Size(75, 23);
 			this.butUninstall.TabIndex = 1;
 			this.butUninstall.Text = "Uninstall";
 			this.butUninstall.UseVisualStyleBackColor = true;
@@ -79,9 +79,9 @@
 			// 
 			// butInstall
 			// 
-			this.butInstall.Location = new System.Drawing.Point(116,113);
+			this.butInstall.Location = new System.Drawing.Point(116, 113);
 			this.butInstall.Name = "butInstall";
-			this.butInstall.Size = new System.Drawing.Size(75,23);
+			this.butInstall.Size = new System.Drawing.Size(75, 23);
 			this.butInstall.TabIndex = 0;
 			this.butInstall.Text = "Install";
 			this.butInstall.UseVisualStyleBackColor = true;
@@ -89,58 +89,61 @@
 			// 
 			// textStatus
 			// 
-			this.textStatus.Location = new System.Drawing.Point(156,82);
+			this.textStatus.Location = new System.Drawing.Point(156, 82);
 			this.textStatus.Name = "textStatus";
 			this.textStatus.ReadOnly = true;
-			this.textStatus.Size = new System.Drawing.Size(217,20);
+			this.textStatus.Size = new System.Drawing.Size(217, 20);
 			this.textStatus.TabIndex = 3;
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(106,83);
+			this.label2.Location = new System.Drawing.Point(106, 83);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(48,18);
+			this.label2.Size = new System.Drawing.Size(48, 18);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Status";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(51,31);
+			this.label1.Location = new System.Drawing.Point(51, 31);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(103,18);
+			this.label1.Size = new System.Drawing.Size(103, 18);
 			this.label1.TabIndex = 5;
 			this.label1.Text = "Service Name";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// textName
 			// 
-			this.textName.Location = new System.Drawing.Point(156,30);
+			this.textName.Location = new System.Drawing.Point(156, 30);
 			this.textName.Name = "textName";
-			this.textName.Size = new System.Drawing.Size(217,20);
+			this.textName.Size = new System.Drawing.Size(217, 20);
 			this.textName.TabIndex = 6;
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(6,57);
+			this.label3.Location = new System.Drawing.Point(6, 57);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(148,18);
+			this.label3.Size = new System.Drawing.Size(148, 18);
 			this.label3.TabIndex = 7;
-			this.label3.Text = "Path to OpenDentHL7.exe";
+			this.label3.Text = "Path to service";
 			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// textPathToExe
 			// 
-			this.textPathToExe.Location = new System.Drawing.Point(156,56);
+			this.textPathToExe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textPathToExe.Location = new System.Drawing.Point(156, 56);
 			this.textPathToExe.Name = "textPathToExe";
-			this.textPathToExe.Size = new System.Drawing.Size(448,20);
+			this.textPathToExe.Size = new System.Drawing.Size(470, 20);
 			this.textPathToExe.TabIndex = 8;
 			// 
 			// butBrowse
 			// 
-			this.butBrowse.Location = new System.Drawing.Point(608,55);
+			this.butBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.butBrowse.Location = new System.Drawing.Point(630, 55);
 			this.butBrowse.Name = "butBrowse";
-			this.butBrowse.Size = new System.Drawing.Size(75,23);
+			this.butBrowse.Size = new System.Drawing.Size(75, 23);
 			this.butBrowse.TabIndex = 9;
 			this.butBrowse.Text = "Browse";
 			this.butBrowse.UseVisualStyleBackColor = true;
@@ -148,9 +151,9 @@
 			// 
 			// FormServiceManage
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F,13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(697,166);
+			this.ClientSize = new System.Drawing.Size(719, 166);
 			this.Controls.Add(this.butBrowse);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.textPathToExe);
--- ServiceManager/FormServiceManage.cs
+++ ServiceManager/FormServiceManage.cs
@@ -14,10 +14,17 @@ using System.IO;
 
 namespace ServiceManager {
 	public partial class FormServiceManage:Form {
+		private FileInfo _serviceFile {
+			get {
+				if(File.Exists(textPathToExe.Text)) {
+					return new FileInfo(textPathToExe.Text);
+				}
+				return null;
+			}
+		}
 		public ServiceController ServControllerCur;
 		public List<ServiceController> allOpenDentServices;
-		public string pathToExe;
-
+		
 		public FormServiceManage() {
 			InitializeComponent();
 		}
@@ -34,10 +41,7 @@ namespace ServiceManager {
 				textName.Text=ServControllerCur.ServiceName;
 				RegistryKey hklm=Registry.LocalMachine;
 				hklm=hklm.OpenSubKey(@"System\CurrentControlSet\Services\"+ServControllerCur.ServiceName);
-				pathToExe=hklm.GetValue("ImagePath").ToString();
-				pathToExe=pathToExe.Replace("\"","");
-				pathToExe=Path.GetDirectoryName(pathToExe);
-				textPathToExe.Text=pathToExe;
+				textPathToExe.Text=hklm.GetValue("ImagePath").ToString().Replace("\"","");
 				textStatus.Text="Installed";
 				butInstall.Enabled=false;
 				butUninstall.Enabled=true;
@@ -69,31 +73,33 @@ namespace ServiceManager {
 		}
 
 		private void butInstall_Click(object sender,EventArgs e) {
+			if(_serviceFile==null) {
+				MessageBox.Show("Select a valid service path");
+				return;
+			}
 			if(textName.Text.Length<8 || textName.Text.Substring(0,8)!="OpenDent") {
 				MessageBox.Show("Error.  Service name must begin with \"OpenDent\".");
 				return;
 			}
 			for(int i=0;i<allOpenDentServices.Count;i++) {//create list of all OpenDent service install paths to ensure only one service can be installed from each directory
 				if(textName.Text==allOpenDentServices[i].ServiceName) {
-					MessageBox.Show("Error.  An OpenDentHL7 service with this name is already installed.  Names must be unique.");
+					MessageBox.Show("Error.  A service with this name is already installed.  Names must be unique.");
 					return;
 				}
 				RegistryKey hklm=Registry.LocalMachine;
 				hklm=hklm.OpenSubKey(@"System\CurrentControlSet\Services\"+allOpenDentServices[i].ServiceName);
-				string installedServicePath=hklm.GetValue("ImagePath").ToString();
-				installedServicePath=installedServicePath.Replace("\"","");
-				installedServicePath=Path.GetDirectoryName(installedServicePath);
-				if(installedServicePath==textPathToExe.Text) {
-					MessageBox.Show("Error.  Cannot install an HL7 service from this directory.  Another OpenDentHL7 service is already installed from this directory.");
+				string installedServicePath=hklm.GetValue("ImagePath").ToString().Replace("\"","");
+				if(installedServicePath==_serviceFile.FullName) {
+					MessageBox.Show("Error.  Cannot install service.  This service is already installed from this directory.");
 					return;
 				}
 			}
 			Process process=new Process();
-			process.StartInfo.WorkingDirectory=textPathToExe.Text;
-			process.StartInfo.FileName="installutil.exe";
+			process.StartInfo.WorkingDirectory=_serviceFile.DirectoryName;
+			process.StartInfo.FileName=Path.Combine(Directory.GetCurrentDirectory(),"installutil.exe");
 			//new strategy for having control over servicename
 			//InstallUtil /ServiceName=OpenDentHL7_abc OpenDentHL7.exe
-			process.StartInfo.Arguments="/ServiceName="+textName.Text+" OpenDentHL7.exe";
+			process.StartInfo.Arguments="/ServiceName="+textName.Text+" "+_serviceFile.Name;
 			process.Start();
 			try {
 				process.WaitForExit(10000);
@@ -114,11 +120,15 @@ namespace ServiceManager {
 		}
 
 		private void butUninstall_Click(object sender,EventArgs e) {
+			if(_serviceFile==null) {
+				MessageBox.Show("Selected service has an invalid path");
+				return;
+			}
 			RegistryKey hklm=Registry.LocalMachine;
 			Process process=new Process();
-			process.StartInfo.WorkingDirectory=textPathToExe.Text;
-			process.StartInfo.FileName="installutil.exe";
-			process.StartInfo.Arguments="/u /ServiceName="+textName.Text+" OpenDentHL7.exe";
+			process.StartInfo.WorkingDirectory=_serviceFile.DirectoryName;
+			process.StartInfo.FileName=Path.Combine(Directory.GetCurrentDirectory(),"installutil.exe");
+			process.StartInfo.Arguments="/u /ServiceName="+textName.Text+" "+_serviceFile.Name;
 			process.Start();
 			try {
 				process.WaitForExit(10000);
@@ -170,13 +180,17 @@ namespace ServiceManager {
 		private void butRefresh_Click(object sender,EventArgs e) {
 			FormServiceManager_Load(this,e);
 		}
-
+				
 		private void butBrowse_Click(object sender,EventArgs e) {
-			FolderBrowserDialog dlg=new FolderBrowserDialog();
-			dlg.SelectedPath=textPathToExe.Text;
-			if(dlg.ShowDialog()==DialogResult.OK) {
-				textPathToExe.Text=dlg.SelectedPath;
+			OpenFileDialog fdlg=new OpenFileDialog();
+			fdlg.Title="Select a Service";
+			fdlg.InitialDirectory=Directory.GetCurrentDirectory();
+			fdlg.Filter="Executable files(*.exe)|*.exe";
+			fdlg.RestoreDirectory=true;
+			if(fdlg.ShowDialog()!=DialogResult.OK) {
+				return;				
 			}
+			textPathToExe.Text=fdlg.FileName;
 		}
 	}
 }
