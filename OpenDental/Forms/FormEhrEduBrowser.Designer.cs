3b88a2724ca80f8bd5ca8a01c089f4ef71b1837d
tgriswold <tgriswold@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-04-18 19:02:15 +0000

    EHR, Educational resource web browser was sometimes displaying script errors.  Changed property on the webbrowser to suppress script errors.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.2@10892 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormEhrEduBrowser.Designer.cs
+++ OpenDental/Forms/FormEhrEduBrowser.Designer.cs
@@ -45,9 +45,11 @@
 			this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
 			this.webBrowser1.Location = new System.Drawing.Point(1, 1);
 			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
 			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.ScriptErrorsSuppressed = true;
 			this.webBrowser1.Size = new System.Drawing.Size(882, 603);
 			this.webBrowser1.TabIndex = 1;
 			// 
