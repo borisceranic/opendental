53b9e28a25bfc62f7af61a118c739dbfae343e50
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-11-06 22:31:24 +0000

    Edit Carrier window would sometimes hide the OK and Cancel buttons. Backported to: 14.2.39.0;14.3.18.0 Reviewed by: Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.3@11524 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormCarrierEdit.cs
+++ OpenDental/Forms/FormCarrierEdit.cs
@@ -712,6 +712,7 @@ namespace OpenDental{
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.butOK);
 			this.Controls.Add(this.butCancel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
