5628419d6809a3ffa367931007ab720826083ffd
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-11-10 22:30:17 +0000

    Replication duplicate primary key error on patientnote table when the same patient was selected from two different offices in a short period of time (rare). Backported to: 14.2.41.0;14.3.20.0 Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@11540 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/PatientNotes.cs
+++ OpenDentBusiness/Data Interface/PatientNotes.cs
@@ -58,6 +58,12 @@ namespace OpenDentBusiness{
 			try {
 				string command = "INSERT INTO patientnote (patnum"
 					+") VALUES('"+patNum+"')";
+				if(DataConnection.DBtype==DatabaseType.MySql) {
+					//We may need to do this in Oracle in the future as well.
+					//If using Replication, then we need to watch for duplicate errors, because the insert is lazy.
+					//Replication servers can insert a patient note with a primary key belonging to another replication server's key range.
+					command+=" ON DUPLICATE KEY UPDATE PatNum='"+patNum+"'";
+				}
 				Db.NonQ(command);
 			}
 			catch {
