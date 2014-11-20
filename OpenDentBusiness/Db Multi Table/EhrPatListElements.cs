f2adad1685ea47a6d76797f45c118565adde1cfd
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-11-20 21:27:38 +0000

    Replication may break if using treatment finder or reports dashboard on multiple computers simultaneously. Backported to: 14.2.43.0;14.3.22.0 Reviewed by:Cameron
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@11574 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Db Multi Table/EhrPatListElements.cs
+++ OpenDentBusiness/Db Multi Table/EhrPatListElements.cs
@@ -291,66 +291,68 @@ namespace OpenDentBusiness {
 		//  return retval;
 		//}
 
-		public static DataTable GetListOrderBy(List<EhrPatListElement> elementList,bool isAsc) {
-			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				return Meth.GetTable(MethodBase.GetCurrentMethod(),elementList,isAsc);
-			}
-			DataTable table=new DataTable();
-			string command="DROP TABLE IF EXISTS tempehrlist";
-			Db.NonQ(command);
-			command="CREATE TABLE tempehrlist SELECT patient.PatNum,patient.LName,patient.FName";
-			for(int i=0;i<elementList.Count;i++) {
-				string compStr=elementList[i].CompareString;
-				switch(elementList[i].Restriction) {
-					case EhrRestrictionType.Birthdate:
-						command+=",patient.Birthdate ";
-						break;
-					case EhrRestrictionType.Problem:
-						command+=",(SELECT diseasedef.ICD9Code FROM disease,diseasedef "
-							+"WHERE disease.PatNum=patient.PatNum "
-							+"AND disease.DiseaseDefNum=diseasedef.DiseaseDefNum "
-							+"AND diseasedef.Icd9code LIKE '"+compStr+"%') `"+compStr+"` ";
-						break;
-					case EhrRestrictionType.LabResult:
-						command+=",(SELECT IFNULL(MAX(ObsValue),0) FROM labresult,labpanel WHERE labresult.LabPanelNum=labpanel.LabPanelNum AND labpanel.PatNum=patient.PatNum AND labresult.TestName='"+compStr+"') `"+compStr+"` ";
-						break;
-					case EhrRestrictionType.Medication:
-						command+=",(SELECT COUNT(*) FROM medication,medicationpat WHERE medicationpat.PatNum=patient.PatNum AND medication.MedicationNum=medicationpat.MedicationNum AND medication.MedName LIKE '%"+compStr+"%') `"+compStr+"` ";
-						break;
-					case EhrRestrictionType.Gender:
-						command+=",patient.Gender ";
-						break;
-				}
-			}
-			command+="FROM patient";
-			Db.NonQ(command);
-			string order="";
-			command="SELECT * FROM tempehrlist ";
-			for(int i=0;i<elementList.Count;i++) {
-				if(i<1) {
-					command+="WHERE "+GetFilteringText(elementList[i]);
-				}
-				else {
-					command+="AND "+GetFilteringText(elementList[i]);
-				}
-				if(elementList[i].OrderBy) {
-					if(elementList[i].Restriction==EhrRestrictionType.Birthdate) {
-						order="ORDER BY Birthdate"+GetOrderBy(isAsc);
-					}
-					else if(elementList[i].Restriction==EhrRestrictionType.Gender) {
-						order="ORDER BY Gender"+GetOrderBy(isAsc);
-					}
-					else {
-						order="ORDER BY `"+POut.String(elementList[i].CompareString)+"`"+GetOrderBy(isAsc);
-					}
-				}
-			}
-			command+=order;
-			table=Db.GetTable(command);
-			command="DROP TABLE IF EXISTS tempehrlist";
-			Db.NonQ(command);
-			return table;
-		}
+		//public static DataTable GetListOrderBy(List<EhrPatListElement> elementList,bool isAsc) {
+		//	if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+		//		return Meth.GetTable(MethodBase.GetCurrentMethod(),elementList,isAsc);
+		//	}
+		//	DataTable table=new DataTable();
+		//	Random rnd=new Random();
+		//	string rndStr=rnd.Next(1000000).ToString();
+		//	string command="DROP TABLE IF EXISTS tempehrlist"+rndStr+@"";
+		//	Db.NonQ(command);
+		//	command="CREATE TABLE tempehrlist"+rndStr+@" SELECT patient.PatNum,patient.LName,patient.FName";
+		//	for(int i=0;i<elementList.Count;i++) {
+		//		string compStr=elementList[i].CompareString;
+		//		switch(elementList[i].Restriction) {
+		//			case EhrRestrictionType.Birthdate:
+		//				command+=",patient.Birthdate ";
+		//				break;
+		//			case EhrRestrictionType.Problem:
+		//				command+=",(SELECT diseasedef.ICD9Code FROM disease,diseasedef "
+		//					+"WHERE disease.PatNum=patient.PatNum "
+		//					+"AND disease.DiseaseDefNum=diseasedef.DiseaseDefNum "
+		//					+"AND diseasedef.Icd9code LIKE '"+compStr+"%') `"+compStr+"` ";
+		//				break;
+		//			case EhrRestrictionType.LabResult:
+		//				command+=",(SELECT IFNULL(MAX(ObsValue),0) FROM labresult,labpanel WHERE labresult.LabPanelNum=labpanel.LabPanelNum AND labpanel.PatNum=patient.PatNum AND labresult.TestName='"+compStr+"') `"+compStr+"` ";
+		//				break;
+		//			case EhrRestrictionType.Medication:
+		//				command+=",(SELECT COUNT(*) FROM medication,medicationpat WHERE medicationpat.PatNum=patient.PatNum AND medication.MedicationNum=medicationpat.MedicationNum AND medication.MedName LIKE '%"+compStr+"%') `"+compStr+"` ";
+		//				break;
+		//			case EhrRestrictionType.Gender:
+		//				command+=",patient.Gender ";
+		//				break;
+		//		}
+		//	}
+		//	command+="FROM patient";
+		//	Db.NonQ(command);
+		//	string order="";
+		//	command="SELECT * FROM tempehrlist"+rndStr+@" ";
+		//	for(int i=0;i<elementList.Count;i++) {
+		//		if(i<1) {
+		//			command+="WHERE "+GetFilteringText(elementList[i]);
+		//		}
+		//		else {
+		//			command+="AND "+GetFilteringText(elementList[i]);
+		//		}
+		//		if(elementList[i].OrderBy) {
+		//			if(elementList[i].Restriction==EhrRestrictionType.Birthdate) {
+		//				order="ORDER BY Birthdate"+GetOrderBy(isAsc);
+		//			}
+		//			else if(elementList[i].Restriction==EhrRestrictionType.Gender) {
+		//				order="ORDER BY Gender"+GetOrderBy(isAsc);
+		//			}
+		//			else {
+		//				order="ORDER BY `"+POut.String(elementList[i].CompareString)+"`"+GetOrderBy(isAsc);
+		//			}
+		//		}
+		//	}
+		//	command+=order;
+		//	table=Db.GetTable(command);
+		//	command="DROP TABLE IF EXISTS tempehrlist"+rndStr+@"";
+		//	Db.NonQ(command);
+		//	return table;
+		//}
 
 		private static string GetOrderBy(bool isAsc) {
 			if(isAsc) {
