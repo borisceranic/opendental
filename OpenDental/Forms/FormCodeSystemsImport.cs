c571ace8116d288d9a693389c7d9aceb4c0ac2e0
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-12-19 00:41:16 +0000

    ICD9 cache was being added twice which caused an error message show. Backported to: 14.3.25.0 Reviewed by: Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.3@11676 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormCodeSystemsImport.cs
+++ OpenDental/Forms/FormCodeSystemsImport.cs
@@ -37,6 +37,8 @@ namespace OpenDental {
 			if(!UpdateCodeSystemThread.IsRunning) { //All done, exit.
 				if(_hasDownloaded) {
 					DataValid.SetInvalid(InvalidType.EhrCodes);//Update in-memory list of codes for all other workstations
+					DataValid.SetInvalid(InvalidType.Diseases);//The purpose of this line is to refresh the ICD9 cache, because the ICD9s do not have an invalid type yet.
+					DataValid.SetInvalid(InvalidType.Sops);
 				}
 				return;
 			}
