9051ae079b0a46db1983e507d144ddc390c076ca
sam <sam@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-12-19 23:41:32 +0000

    Added code comments for thread-safe cache pattern to ProviderC
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@11683 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Cache/ProviderC.cs
+++ OpenDentBusiness/Cache/ProviderC.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace OpenDentBusiness {
 	public class ProviderC {
+		//The comments in ProviderC are relevant for all cache classes. However, it is not necessary to add such comments to each cache class.
+		//See wiki [[Programming Pattern - Cache]] for more details on the cache pattern.
+		///<summary>Typically holds the "full" list of cached objects. See comments for _lock.</summary>
 		private static List<Provider> _listLong;
+		///<summary>Typically holds the "short" list of cached objects. See comments for _lock.</summary>
 		private static List<Provider> _listShort;
+		///<summary>Thread safe lock object. Any time you access _listShort or _listLong you MUST wrap the code block with lock(_lock). Failing to lock will result in a potential for unsafe access by multiple threads at the same time.</summary>
 		private static object _lock=new object();
 
-		///<summary>Rarely used. Includes all providers, even if hidden.</summary>
+		///<summary>Rarely used. Includes all providers, even if hidden. Thread-safe. Use GetCacheLong() to get the long list of cached objects. This "getter" is only here for backwards compatibility.</summary>
 		public static List<Provider> ListLong {
 			get {
 				return GetListLong();
@@ -21,7 +26,7 @@ namespace OpenDentBusiness {
 			}
 		}
 
-		///<summary>This is the list used most often. It does not include hidden providers.</summary>
+		///<summary>This is the list used most often. It does not include hidden providers. Thread-safe. Use GetCacheLong() to get the long list of cached objects. This "getter" is only here for backwards compatibility.</summary>
 		public static List<Provider> ListShort {
 			get {
 				if(_listShort==null) {
@@ -36,7 +41,9 @@ namespace OpenDentBusiness {
 			}
 		}
 
+		///<summary>Thread-safe. Returns a copy of the currently cached long list of objects.</summary>
 		public static List<Provider> GetListLong() {
+			//If this is first-time access then the cache will be null. Check for null do the initial RefreshCache when necessary.
 			bool hasNullList=false;
 			lock(_lock) {
 				hasNullList=_listLong==null;
@@ -53,7 +60,9 @@ namespace OpenDentBusiness {
 			return list;
 		}
 
+		///<summary>Thread-safe. Returns a copy of the currently cached short list of objects.</summary>
 		public static List<Provider> GetListShort() {
+			//If this is first-time access then the cache will be null. Check for null do the initial RefreshCache when necessary.
 			bool hasNullList=false;
 			lock(_lock) {
 				hasNullList=_listShort==null;
