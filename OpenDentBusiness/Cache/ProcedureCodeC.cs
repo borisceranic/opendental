216232f5e3c2e96b742741d166700c6c5565bce4
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2014-12-27 00:09:23 +0000

    Save commit for ProcedureCodeC cache pattern to go over with Jordan. - This will be a good example class to show because it will not contain all the detail locking comments like ProviderC.  Also, instead of having a listShort, it has a Hashtable which will show how the new get methods can easily adhere to different object types.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@11710 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Cache/ProcedureCodeC.cs
+++ OpenDentBusiness/Cache/ProcedureCodeC.cs
@@ -32,45 +32,38 @@ namespace OpenDentBusiness {
 			}
 		}
 
-		///<summary>Thread-safe.  Returns a copy of the currently cached long lists of objects.</summary>
+		///<summary></summary>
 		public static List<ProcedureCode> GetListLong() {
 			bool hasNullList=false;
 			lock(_lock) {
-				hasNullList=_list==null;
+				if(_hList==null) {
+					hasNullList=true;
+				}
 			}
 			if(hasNullList) {
 				ProcedureCodes.RefreshCache();
 			}
 			List<ProcedureCode> listProcedureCodes=new List<ProcedureCode>();
 			lock(_lock) {
-				if(_list!=null) {
-					listProcedureCodes.AddRange(_list);
-				}
+				listProcedureCodes.AddRange(_list);
 			}
 			return listProcedureCodes;
 		}
 
-		///<summary>Thread-safe.  Returns a copy of the currently cached long list of objects.</summary>
+		///<summary>key:ProcCode, value:ProcedureCode</summary>
 		public static Hashtable GetHList() {
 			bool hasNullList=false;
 			lock(_lock) {
-				hasNullList=_hList==null;
+				if(_hList==null) {
+					hasNullList=true;
+				}
 			}
 			if(hasNullList) {
 				ProcedureCodes.RefreshCache();
 			}
-			Hashtable hashProcedureCodes=new Hashtable();
-			List<ProcedureCode> listProcedureCodes=GetListLong();
+			Hashtable hashProcedureCodes;
 			lock(_lock) {
-				if(_list!=null) {
-					for(int i=0;i<listProcedureCodes.Count;i++) {
-						try {//Reconstruct the hash table from listProcedureCodes because I don't know how to deep copy a hash table.
-							hashProcedureCodes.Add(listProcedureCodes[i].ProcCode,listProcedureCodes[i].Copy());
-						}
-						catch {//in case of duplicate in db
-						}
-					}
-				}
+				hashProcedureCodes=new Hashtable(_hList);
 			}
 			return hashProcedureCodes;
 		}
