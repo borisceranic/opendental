4180da29ec1d81b4ac46e156e262c76354b3e74a
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-01-16 00:03:49 +0000

    EHR dashboard was not showing measure type Smoking as green for patients under 13 years of age. Backported to: 14.3.28.0 Reviewed by: Cameron - Change made for MU 1 & 2.  Only a visual change for the EHR dashboard.  Does not affect any calculations whatsoever.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@11787 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/EhrMeasures.cs
+++ OpenDentBusiness/Data Interface/EhrMeasures.cs
@@ -2130,6 +2130,9 @@ namespace OpenDentBusiness{
 							mu.Details="Smoking status entered";
 							mu.Met=MuMet.True;
 						}
+						if(pat.Age<13) {//The smoking measure does not apply to patients under the age of 13.
+							mu.Met=MuMet.NA;
+						}
 						mu.Action="Edit smoking status";
 						break;
 					#endregion
@@ -4215,6 +4218,9 @@ namespace OpenDentBusiness{
 						else {
 							mu.Details="Smoking status entered";
 							mu.Met=MuMet.True;
+						}
+						if(pat.Age<13) {//The smoking measure does not apply to patients under the age of 13.
+							mu.Met=MuMet.NA;
 						}
 						mu.Action="Edit smoking status";
 						break;
