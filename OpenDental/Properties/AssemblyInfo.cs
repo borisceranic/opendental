009919256e028bdba7c6001fead7034d3a35b3d4
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-02-26 17:42:04 +0000

    version 14.3.35
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental14.3@11987 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Properties/AssemblyInfo.cs
+++ OpenDental/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("14.3.34.0")]
+[assembly: AssemblyVersion("14.3.35.0")]
 
 //[assembly: AssemblyFileVersion("1.0.0.0")]
