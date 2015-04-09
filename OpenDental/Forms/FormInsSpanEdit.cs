9a8f797ee1af30ad42408b8af8b933125a33052d
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-04-09 22:27:18 +0000

    Canadian insurance category setup, set to defaults button was creating ADA code spans, now creates CDA code spans. Backported to: 14.3.39.0;15.1.20.0 Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12105 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormInsSpanEdit.cs
+++ OpenDental/Forms/FormInsSpanEdit.cs
@@ -54,68 +54,68 @@ namespace OpenDental{
 			// 
 			// textTo
 			// 
-			this.textTo.Location = new System.Drawing.Point(131,34);
+			this.textTo.Location = new System.Drawing.Point(131, 34);
 			this.textTo.Name = "textTo";
-			this.textTo.Size = new System.Drawing.Size(101,20);
+			this.textTo.Size = new System.Drawing.Size(101, 20);
 			this.textTo.TabIndex = 1;
 			// 
 			// textFrom
 			// 
-			this.textFrom.Location = new System.Drawing.Point(15,34);
+			this.textFrom.Location = new System.Drawing.Point(15, 34);
 			this.textFrom.Name = "textFrom";
-			this.textFrom.Size = new System.Drawing.Size(100,20);
+			this.textFrom.Size = new System.Drawing.Size(100, 20);
 			this.textFrom.TabIndex = 0;
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(131,16);
+			this.label2.Location = new System.Drawing.Point(131, 16);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(100,16);
+			this.label2.Size = new System.Drawing.Size(100, 16);
 			this.label2.TabIndex = 8;
-			this.label2.Text = "To ADA";
+			this.label2.Text = "To Code";
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(13,16);
+			this.label3.Location = new System.Drawing.Point(13, 16);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(100,16);
+			this.label3.Size = new System.Drawing.Size(100, 16);
 			this.label3.TabIndex = 9;
-			this.label3.Text = "From ADA";
+			this.label3.Text = "From Code";
 			// 
 			// butOK
 			// 
-			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butOK.Autosize = true;
 			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butOK.CornerRadius = 4F;
-			this.butOK.Location = new System.Drawing.Point(246,88);
+			this.butOK.Location = new System.Drawing.Point(246, 88);
 			this.butOK.Name = "butOK";
-			this.butOK.Size = new System.Drawing.Size(75,26);
+			this.butOK.Size = new System.Drawing.Size(75, 26);
 			this.butOK.TabIndex = 3;
 			this.butOK.Text = "&OK";
 			this.butOK.Click += new System.EventHandler(this.butOK_Click);
 			// 
 			// butCancel
 			// 
-			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butCancel.Autosize = true;
 			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butCancel.CornerRadius = 4F;
 			this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.butCancel.Location = new System.Drawing.Point(246,124);
+			this.butCancel.Location = new System.Drawing.Point(246, 124);
 			this.butCancel.Name = "butCancel";
-			this.butCancel.Size = new System.Drawing.Size(75,26);
+			this.butCancel.Size = new System.Drawing.Size(75, 26);
 			this.butCancel.TabIndex = 4;
 			this.butCancel.Text = "&Cancel";
 			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
 			// 
 			// butDelete
 			// 
-			this.butDelete.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butDelete.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.butDelete.Autosize = true;
 			this.butDelete.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
@@ -123,9 +123,9 @@ namespace OpenDental{
 			this.butDelete.CornerRadius = 4F;
 			this.butDelete.Image = global::OpenDental.Properties.Resources.deleteX;
 			this.butDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butDelete.Location = new System.Drawing.Point(12,124);
+			this.butDelete.Location = new System.Drawing.Point(12, 124);
 			this.butDelete.Name = "butDelete";
-			this.butDelete.Size = new System.Drawing.Size(86,26);
+			this.butDelete.Size = new System.Drawing.Size(86, 26);
 			this.butDelete.TabIndex = 11;
 			this.butDelete.Text = "&Delete";
 			this.butDelete.Click += new System.EventHandler(this.butDelete_Click);
@@ -133,9 +133,9 @@ namespace OpenDental{
 			// FormInsSpanEdit
 			// 
 			this.AcceptButton = this.butOK;
-			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
 			this.CancelButton = this.butCancel;
-			this.ClientSize = new System.Drawing.Size(348,168);
+			this.ClientSize = new System.Drawing.Size(348, 168);
 			this.Controls.Add(this.butDelete);
 			this.Controls.Add(this.butCancel);
 			this.Controls.Add(this.butOK);
