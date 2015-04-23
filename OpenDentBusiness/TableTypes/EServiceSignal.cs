e1503c6405a7da1bed684d81f0b81c77982ddc79
sam <sam@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-04-23 19:47:55 +0000

    Altered EServiceSignal table. Did not run CRUD. Handing off to Ryan.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12156 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/EServiceSignal.cs
+++ OpenDentBusiness/TableTypes/EServiceSignal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace OpenDentBusiness {
 	///<summary>Communication item from OD Cloud to workstation.</summary>
 	[Serializable]
@@ -7,18 +8,21 @@ namespace OpenDentBusiness {
 		///<summary>Primary key.</summary>
 		[CrudColumn(IsPriKey=true)]
 		public long EServiceSignalNum;
-		///<summary>The reason for the eServiceSignal. This code is used to determine what actions to take and how to process this message.</summary>
+		///<summary>Service which this signal applies to. Defined by eServiceCodes.</summary>
+		public int ServiceCode;
+		///<summary>Category defined by ReasonCodeCategories. Can be zero if no grouping is necessary per a given service. Stored as an int for forward compatibility.</summary>
+		public int ReasonCategory;
+		///<summary>The reason for the eServiceSignal. This code is used to determine what actions to take and how to process this message. 
+		///It is a function of ReasonCategory. It will most likely be defined by an enum that lives on HQ-only closed source.</summary>
 		public int ReasonCode;
 		///<summary>Adverse reaction description.</summary>
-		public eServiceStatus SeverityStatus;
+		public eServiceStatus Severity;
 		///<summary>Human readable description of what this signal means, or a message for the user.</summary>
 		public string Description;
 		///<summary>Time signal was sent.</summary>
 		[CrudColumn(SpecialType=CrudSpecialColType.DateT)]
 		public DateTime TimeStamp;
-		///<summary>Used to store serialized data that can be used for processing this signal. 
-		///For instance, if a SMSNumberAssigned message (ReasonCode 20001) then this would contain serialized data 
-		///about the new VLN that is to be assigned and inserted into the local DB.</summary>
+		///<summary>Used to store serialized data that can be used for processing this signal.</summary>
 		public string Tag;
 		///<summary>After a message has been processed or acknowledged this is set true.</summary>
 		public bool IsProcessed;
@@ -28,35 +32,7 @@ namespace OpenDentBusiness {
 			return (EServiceSignal)this.MemberwiseClone();
 		}
 	}
-
-
-	/////<summary>Communication item bound for workstation.</summary>
-	//[Serializable]
-	//public class EServiceSignalHQ:TableBase {
-	//	///<summary>Primary key.</summary>
-	//	[CrudColumn(IsPriKey=true)]
-	//	public long EServiceSignalHQNum;
-	//	public long CustPatNum;
-	//	///<summary>The reason for the eServiceSignal. This code is used to determine what actions to take and how to process this message.</summary>
-	//	public int ReasonCode;
-	//	///<summary>Adverse reaction description.</summary>
-	//	public eServiceStatus Severity;
-	//	///<summary>Human readable description of what this signal means, or a message for the user.</summary>
-	//	public string Description;
-	//	///<summary>The historical date that the patient had the adverse reaction to this agent.</summary>
-	//	public DateTime TimeStamp;
-	//	///<summary>Used to store serialized data that can be used for processing this signal. For instance, if a SMSNumberAssigned message (ReasonCode 20001) 
-	//	///then this would contain serialized data about the new VLN that is to be assigned and inserted into the local DB.</summary>
-	//	public string Tag;
-	//	///<summary>After a message has been processed or acknowledged this is set true.</summary>
-	//	public bool IsProcessed;
-
-	//	///<summary></summary>
-	//	public EServiceSignal Copy() {
-	//		return (EServiceSignal)this.MemberwiseClone();
-	//	}
-	//}
-
+	
 	///<summary>Used to determine that status of the entire service. 
 	///For example, if there is a Listener eServiceSignal (ReasonCode 10000 to 19999) and a Status of Error, 
 	///then the entire listener service is considered to be in the error state.</summary>
@@ -66,5 +42,12 @@ namespace OpenDentBusiness {
 		Warning,
 		Error,
 		Critical
+	}
+
+	///<summary>Used by EServiceSignal.ServiceCode. Each service will have an entry here. Stored as an int for forward compatibility.</summary>
+	public enum eServiceCodes {
+		Undefined=0,
+		ListenerService=1,
+		SMSService=2
 	}
 }
