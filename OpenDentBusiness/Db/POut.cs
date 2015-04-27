70d8afce5431e68a7eb1a330ad33579c65e4ff5a
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-04-27 19:01:42 +0000

    Reverted previous POut.StringNote change since making it work would take too much extra code. Added a comment to never use POut.StringNote outside of the CRUD. Comment made to the Head only.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.1@12186 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Db/POut.cs
+++ OpenDentBusiness/Db/POut.cs
@@ -227,7 +227,7 @@ namespace OpenDentBusiness{
 			myString=Regex.Replace(myString,@"[\n]{50,}","\n");//{50,} means 50 or more.
 			myString=Regex.Replace(myString,@"[\t]{50,}","\t");
 			myString=myString.Replace("\n","\r\n");
-			return String(myString);
+			return myString;
 		}
 
 		//<summary></summary>
