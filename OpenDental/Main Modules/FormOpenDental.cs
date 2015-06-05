d04a4425540a364a02a48df3cee150d460aeb3a8
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-06-05 00:06:46 +0000

    Enabled PhoneConf tracking. Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.2@12341 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/FormOpenDental.cs
+++ OpenDental/Main Modules/FormOpenDental.cs
@@ -4449,7 +4449,9 @@ namespace OpenDental{
 			}
 			//typically every 4 seconds:
 			ProcessSignals();
-			//lightSignalGrid1.SetConfs(PhoneConfs.GetAll());
+			if(PrefC.GetBool(PrefName.DockPhonePanelShow)) {
+				lightSignalGrid1.SetConfs(PhoneConfs.GetAll());
+			}
 		}
 
 		private void timerDisabledKey_Tick(object sender,EventArgs e) {
