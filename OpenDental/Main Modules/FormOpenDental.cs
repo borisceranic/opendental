66030fd1b4062128629508fbdf2e1109b20a1c33
josh <josh@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-06-22 20:15:27 +0000

    Request #3873 now accounts for eCW mode when selecting a default module. Reviewed by:  Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12438 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/FormOpenDental.cs
+++ OpenDental/Main Modules/FormOpenDental.cs
@@ -6426,7 +6426,10 @@ namespace OpenDental{
 					MsgBox.Show(this,"You do not have permission to use any modules.");
 				}
 			}
-			if(startingModuleIdx!=-1 && startingModuleIdx==Security.GetModule(startingModuleIdx)) {
+			if(startingModuleIdx!=-1//A starting module was requested
+				&& startingModuleIdx==Security.GetModule(startingModuleIdx)//and the user has access to the specified module
+				&& myOutlookBar.Buttons[startingModuleIdx].Visible)//and the module is an available option (ex would not be availble for eCW tight/full).
+			{
 				UnselectActive();
 				allNeutral();//Sets all controls to false.  Needed to set the new module as selected.
 				myOutlookBar.SelectedIndex=startingModuleIdx;
