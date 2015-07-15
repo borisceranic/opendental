242875922f5f2aa548cb5e19bba5e33ec2f5ae2c
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-07-15 01:42:43 +0000

    Added return StatusCode to podium bridge.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12557 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Bridges/Podium.cs
+++ OpenDental/Bridges/Podium.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
-namespace OpenDental.Bridges{
-
-	///<summary>RESTful bridge to podium service.</summary>
+namespace OpenDental.Bridges {
+	///<summary>RESTful bridge to podium service. Without using REST Sharp or JSON libraries this code might not work properly.</summary>
 	public class Podium {
 
 		///<summary></summary>
@@ -13,37 +13,45 @@ namespace OpenDental.Bridges{
 			
 		}
 
-		public static void SendInvitation(string phoneNumber,string firstName,string lastName,string emailIn,bool isTest) {
+		///<summary>Tries each of the phone numbers provided in the list one at a time until it succeeds.</summary>
+		public static bool SendInvitation(List<string> listPhoneNumbers,string firstName,string lastName,string emailIn,bool isTest) {
 			try {
-				string apiUrl=""; //todo: get from program pref
-				string apiToken=""; //todo: get from program pref
-				string locationId=""; //todo: get from program pref
-				using(WebClient client=new WebClient()) {
-					client.Headers[HttpRequestHeader.Accept]="application/json";
-					client.Headers[HttpRequestHeader.ContentType]="application/json";
-					client.Headers[HttpRequestHeader.Authorization]="Token token=\""+apiToken+"\"";
-					client.Encoding=UnicodeEncoding.UTF8;
-					string bodyJson=string.Format(@"
-					{{
-						""location_id"": ""{0}"",
-						""phone_number"": ""{1}"",
-						""customer"": {{
-							""first_name"": ""{2}"",
-							""last_name"": ""{3}"",
-							""emailIn"": ""{4}""
-						}},
-						""test"": {5}
-					}}",locationId,phoneNumber,firstName,lastName,emailIn,isTest);
-					//Post with Authorization headers and a body comprised of a JSON serialized anonymous type.
-					client.UploadString(apiUrl,"POST",bodyJson);
+				for(int i=0;i<listPhoneNumbers.Count;i++) {
+					string apiUrl=""; //todo: Hard coded per api documentation
+					string apiToken=""; //todo: get from program pref
+					string locationId=""; //todo: get from program pref
+					using(WebClientEx client=new WebClientEx()) {
+						client.Headers[HttpRequestHeader.Accept]="application/json";
+						client.Headers[HttpRequestHeader.ContentType]="application/json";
+						client.Headers[HttpRequestHeader.Authorization]="Token token=\""+apiToken+"\"";
+						client.Encoding=UnicodeEncoding.UTF8;
+						string bodyJson=string.Format(@"
+						{{
+							""location_id"": ""{0}"",
+							""phone_number"": ""{1}"",
+							""customer"": {{
+								""first_name"": ""{2}"",
+								""last_name"": ""{3}"",
+								""emailIn"": ""{4}""
+							}},
+							""test"": {5}
+						}}",locationId,listPhoneNumbers[i],firstName,lastName,emailIn,isTest);
+						//Post with Authorization headers and a body comprised of a JSON serialized anonymous type.
+						client.UploadString(apiUrl,"POST",bodyJson);
+						if(client.StatusCode==HttpStatusCode.OK) {
+							return true;
+						}
+					}
 				}
 			}
 			catch (Exception ex) {
-				MessageBox.Show("Error sending to Podium.\r\n"+ex.Message);
+				MessageBox.Show(Lan.g("Podium","Error sending to Podium.")+"\r\n"+ex.Message);
 			}
+			//explicitly failed or did not succeed.
+			return false;
 			//Sample Request:
 
-			//Accept: 'application/json'
+			//Accept: 'application/json's
 			//Content-Type: 'application/json'
 			//Authorization: 'Token token="my_dummy_token"'
 			//Body:
@@ -59,6 +67,22 @@ namespace OpenDental.Bridges{
 			//}
 		}
 
+	}
+
+	class WebClientEx:WebClient {
+		//http://stackoverflow.com/questions/3574659/how-to-get-status-code-from-webclient
+		private WebResponse _mResp = null;
+
+		protected override WebResponse GetWebResponse(WebRequest req,IAsyncResult ar) {
+			return _mResp = base.GetWebResponse(req,ar);
+		}
+
+		public HttpStatusCode StatusCode {
+			get {
+				HttpWebResponse httpWebResponse=_mResp as HttpWebResponse;
+				return httpWebResponse!=null?httpWebResponse.StatusCode:HttpStatusCode.OK;
+			}
+		}
 	}
 }
 
