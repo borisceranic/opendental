f903a66759a089991dc8bcecd053a3da57f308ac
josh <josh@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-07-21 20:50:56 +0000

    Payor IDs window would sometimes throw an unhandled exception if no carrier was selected. Backported to: 15.2.13.0;15.3.3.0 Reviewed by:  Allen, Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12615 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormElectIDs.cs
+++ OpenDental/Forms/FormElectIDs.cs
@@ -206,11 +206,13 @@ namespace OpenDental{
 		}
 
 		private void butOK_Click(object sender, System.EventArgs e) {
-			if(IsSelectMode && gridElectIDs.SelectedIndices.Length<1){
-				MessageBox.Show(Lan.g(this,"Please select an item first."));
-				return;
+			if(IsSelectMode) {
+				if(gridElectIDs.SelectedIndices.Length<1) {
+					MessageBox.Show(Lan.g(this,"Please select an item first."));
+					return;
+				}
+				selectedID=ElectIDs.List[gridElectIDs.SelectedIndices[0]];
 			}
-			selectedID=ElectIDs.List[gridElectIDs.SelectedIndices[0]];
 			DialogResult=DialogResult.OK;
 		}
 
