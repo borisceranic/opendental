19cf6199710ddaa6a32098b4a8a0b6773282fc57
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-07-21 22:18:43 +0000

    version 15.3.3
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.3@12616 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Properties/AssemblyInfo.cs
+++ OpenDental/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("15.3.2.0")]
+[assembly: AssemblyVersion("15.3.3.0")]
 
 //[assembly: AssemblyFileVersion("1.0.0.0")]
