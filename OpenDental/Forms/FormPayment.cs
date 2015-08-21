2e4dbf5bda96d2b54965d0436a99f961fcc7f4c9
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-08-21 17:22:52 +0000

    Payment window, after closing the Pay Split Manager the Payment amount would change sometimes. Backported to: 15.2.17.0;15.3.11.0 Reviewed by:Allen and Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12800 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormPayment.cs
+++ OpenDental/Forms/FormPayment.cs
@@ -1958,8 +1958,6 @@ namespace OpenDental {
 			FormPSM.ShowDialog();
 			if(FormPSM.DialogResult==DialogResult.OK) {
 				SplitList=FormPSM.ListSplitsCur;
-				PaymentCur=FormPSM.PaymentCur;
-				textAmount.Text=POut.Double(PaymentCur.PayAmt);
 			}
 			FillMain();
 		}
@@ -2119,21 +2117,18 @@ namespace OpenDental {
 					FormPSM.PaymentAmt=PIn.Double(textAmount.Text);
 					FormPSM.FamCur=Patients.GetFamily(PatCur.PatNum);
 					FormPSM.PatCur=PatCur;
-					PaymentCur.PayAmt=0;//Fix for PSM logic depending on new payments having 0 amount.
-					FormPSM.PaymentCur=PaymentCur;
+					FormPSM.PaymentCur=PaymentCur.Clone();
 					FormPSM.PayDate=PIn.DateT(textDate.Text);
 					FormPSM.IsNew=IsNew;
-					FormPSM.ListSplitsCur=SplitList;
+					FormPSM.ListSplitsCur=new List<PaySplit>();
 					if(FormPSM.ShowDialog()==DialogResult.OK) {
 						SplitList=FormPSM.ListSplitsCur;
-						PaymentCur=FormPSM.PaymentCur;
 						if(SplitList.Count==0) {//If they clicked OK without any splits being added, add one split.
 							AddOneSplit();
 						}
 					}
 					else {//Cancel
-						SplitList.Clear();//This is OK, they got this far because it's new and had no splits to begin with.
-						AddOneSplit();
+						AddOneSplit();//Someone decided to add a split and call it good if the user cancels out of the manager window.
 					}
 				}
 				else {//Either no allocation required, or user does not want to allocate.  Just add one split.
