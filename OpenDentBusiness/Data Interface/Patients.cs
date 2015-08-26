7878139345ca81078fe179934ea9c8f183a31bd6
chris <chris@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-08-26 21:30:14 +0000

    Changed the Patient Select filtering back to how it was before 15.1. When filtering by clinics, it no longer includes patients who had a completed or TP procedure at that clinic. Reviewed by: Cameron Backported to: 15.2.19, 15.3.13
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12826 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/Patients.cs
+++ OpenDentBusiness/Data Interface/Patients.cs
@@ -396,13 +396,16 @@ namespace OpenDentBusiness{
 				command+="AND patient.PatNum = patient.Guarantor ";
 			}
 			if(clinicNums!="") {
-				command+="AND patient.Guarantor IN ( "
-				+"SELECT DISTINCT Guarantor FROM patient "
-				+"LEFT JOIN procedurelog ON patient.PatNum=procedurelog.PatNum "
-					+"AND (procedurelog.ProcStatus="+POut.Int((int)ProcStat.TP)+" OR procedurelog.ProcStatus="+POut.Int((int)ProcStat.C)+") "
-					+"AND procedurelog.ClinicNum IN ("+POut.String(clinicNums)+") "
-				+"WHERE patient.PatStatus !="+POut.Int((int)PatientStatus.Deleted)+" "
-				+"AND (procedurelog.PatNum IS NOT NULL OR patient.ClinicNum IN (0,"+POut.String(clinicNums)+"))) "; //Includes patients that are not assigned to any clinic.  May need to restrict selection of these patients in the future.
+				//Checking for completed or TP procedures was taking too long for large databases. We may revisit this in the future.
+				//command+="AND patient.Guarantor IN ( "
+				//+"SELECT DISTINCT Guarantor FROM patient "
+				//+"LEFT JOIN procedurelog ON patient.PatNum=procedurelog.PatNum "
+				//	+"AND (procedurelog.ProcStatus="+POut.Int((int)ProcStat.TP)+" OR procedurelog.ProcStatus="+POut.Int((int)ProcStat.C)+") "
+				//	+"AND procedurelog.ClinicNum IN ("+POut.String(clinicNums)+") "
+				//+"WHERE patient.PatStatus !="+POut.Int((int)PatientStatus.Deleted)+" "
+				//+"AND (procedurelog.PatNum IS NOT NULL OR patient.ClinicNum IN (0,"+POut.String(clinicNums)+"))) "; //Includes patients that are not assigned to any clinic.  May need to restrict selection of these patients in the future.
+				//Only include patients who are assigned to the clinic and also patients who are not assigned to any clinic
+				command+="AND patient.ClinicNum IN (0,"+POut.String(clinicNums)+") ";
 			}
 			if(siteNum>0) {
 				command+="AND patient.SiteNum="+POut.Long(siteNum)+" ";
