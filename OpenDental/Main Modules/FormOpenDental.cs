c4ff29e2303cf78d47f9d1ebf9962201c030e800
chris <chris@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-08-27 18:18:32 +0000

    When logging off and logging back on, if the user is restricted to one clinic, shows that clinic instead of the computer pref clinic. Backported to: 15.2.20 and 15.3.13 Reviewed by: Cameron, Allen
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@12834 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/FormOpenDental.cs
+++ OpenDental/Main Modules/FormOpenDental.cs
@@ -7021,7 +7021,12 @@ namespace OpenDental{
 			myOutlookBar.SelectedIndex=Security.GetModule(LastModule);
 			myOutlookBar.Invalidate();
 			if(!PrefC.GetBool(PrefName.EasyNoClinics)) {
-				ClinicNum=ComputerPrefs.LocalComputer.ClinicNum;
+				if(Security.CurUser.ClinicIsRestricted) {
+					ClinicNum=Security.CurUser.ClinicNum;
+				}
+				else {
+					ClinicNum=ComputerPrefs.LocalComputer.ClinicNum;
+				}
 				Clinics.ClinicNum=ClinicNum;
 				RefreshMenuClinics();
 			}
