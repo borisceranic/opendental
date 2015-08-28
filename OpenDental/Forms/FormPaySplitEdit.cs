61edc15d2d438f57312f9c8baa7a76c446a2dc70
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-08-28 16:54:43 +0000

    Pay Split Edit enhancement, when the remaining amount is negative due to an overpayment on a procedure a warning will display allowing the user to change the amount or continue. Backported to: 15.2.20.0;15.3.13.0 Reviewed by: Allen and Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.3@12843 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormPaySplitEdit.cs
+++ OpenDental/Forms/FormPaySplitEdit.cs
@@ -84,7 +84,7 @@ namespace OpenDental
 		private ComboBox comboClinic;
 		private Label labelClinic;
 		private double ProcPaidHere;
-
+		private double _remainAmt;
 
 		///<summary></summary>
 		public FormPaySplitEdit(Family famCur){//PaySplit paySplitCur,Family famCur){
@@ -950,7 +950,7 @@ namespace OpenDental
 				textProcPaidHere.Text=ProcPaidHere.ToString("F");
 			}
 			//most of these are negative values, so add
-			double remain=
+			_remainAmt=
 				ProcFee
 				+ProcWriteoff
 				+ProcInsPaid
@@ -958,7 +958,7 @@ namespace OpenDental
 				+ProcAdj
 				+ProcPrevPaid
 				+ProcPaidHere;
-			labelProcRemain.Text=remain.ToString("c");
+			labelProcRemain.Text=_remainAmt.ToString("c");
 		}
 
 		private void textAmount_Validating(object sender, System.ComponentModel.CancelEventArgs e) {
@@ -1057,6 +1057,11 @@ namespace OpenDental
 			if(amount==0) {
 				MsgBox.Show(this,"Please enter an amount");
 				return;
+			}
+			if(_remainAmt<0) {
+				if(!MsgBox.Show(this,true,"Warning: Remaining amount is negative.  Continue?")) {
+					return;
+				}
 			}
 			if(checkPayPlan.Checked && checkPatOtherFam.Checked){
 				MessageBox.Show(Lan.g(this,"You cannot split outside of the family for a payment plan.")+"  "
