2cc0280dfe68269ad0fe55379a2655e8245580c4
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-08-28 17:02:01 +0000

    Payment window, if the Pay Split Manager preference is enabled, zero amounts on the payment would cause an error to display when pressing OK. Backported to: 15.2.20.0;15.3.13.0 Reviewed by: Allen and Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.3@12844 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormPayment.cs
+++ OpenDental/Forms/FormPayment.cs
@@ -1824,6 +1824,7 @@ namespace OpenDental {
 			FormPSM.ShowDialog();
 			if(FormPSM.DialogResult==DialogResult.OK) {
 				SplitList=FormPSM.ListSplitsCur;
+				textAmount.Text=FormPSM.AmtTotal.ToString("F");
 			}
 			FillMain();
 		}
@@ -1887,7 +1888,7 @@ namespace OpenDental {
 					MessageBox.Show(Lan.g(this,"Please enter an amount."));
 					return;
 				}
-				if(PIn.Double(textAmount.Text)==0) {
+				if(PIn.Double(textAmount.Text)==0 && !PrefC.GetBool(PrefName.PaymentsPromptForAutoSplit)) {
 					MessageBox.Show(Lan.g(this,"Amount must not be zero unless this is a transfer."));
 					return;
 				}
@@ -1989,6 +1990,7 @@ namespace OpenDental {
 					FormPSM.ListSplitsCur=new List<PaySplit>();
 					if(FormPSM.ShowDialog()==DialogResult.OK) {
 						SplitList=FormPSM.ListSplitsCur;
+						PaymentCur.PayAmt=FormPSM.AmtTotal;
 						if(SplitList.Count==0) {//If they clicked OK without any splits being added, add one split.
 							AddOneSplit();
 						}
