d0abb5985194197b2257a63ed56c9293a2caaace
chris <chris@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-10-02 22:55:35 +0000

    Request #4166 (partial) - Added clinic filter to Procedures Not Billed to Insurance and switched it to use Report Complex.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13020 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms Reports/FormRpProcNotBilledIns.cs
+++ OpenDental/Forms Reports/FormRpProcNotBilledIns.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Drawing;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Forms;
 using OpenDentBusiness;
+using System.Data;
+using OpenDental.ReportingComplex;
 
 namespace OpenDental{
 ///<summary></summary>
@@ -15,7 +18,11 @@ namespace OpenDental{
 		private OpenDental.UI.Button butOK;
 		private System.ComponentModel.Container components = null;
 		private CheckBox checkMedical;
+		private CheckBox checkAllClin;
+		private ListBox listClin;
+		private Label labelClin;
 		private FormQuery FormQuery2;
+		private List<Clinic> _listClinics;
 
 		///<summary></summary>
 		public FormRpProcNotBilledIns(){
@@ -50,17 +57,20 @@ namespace OpenDental{
 			this.butCancel = new OpenDental.UI.Button();
 			this.butOK = new OpenDental.UI.Button();
 			this.checkMedical = new System.Windows.Forms.CheckBox();
+			this.checkAllClin = new System.Windows.Forms.CheckBox();
+			this.listClin = new System.Windows.Forms.ListBox();
+			this.labelClin = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// date2
 			// 
-			this.date2.Location = new System.Drawing.Point(288, 112);
+			this.date2.Location = new System.Drawing.Point(289, 18);
 			this.date2.Name = "date2";
 			this.date2.TabIndex = 2;
 			// 
 			// date1
 			// 
-			this.date1.Location = new System.Drawing.Point(32, 112);
+			this.date1.Location = new System.Drawing.Point(32, 18);
 			this.date1.Name = "date1";
 			this.date1.TabIndex = 1;
 			// 
@@ -76,12 +86,13 @@ namespace OpenDental{
 			// butCancel
 			// 
 			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butCancel.Autosize = true;
 			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butCancel.CornerRadius = 4F;
 			this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.butCancel.Location = new System.Drawing.Point(520, 328);
+			this.butCancel.Location = new System.Drawing.Point(478, 352);
 			this.butCancel.Name = "butCancel";
 			this.butCancel.Size = new System.Drawing.Size(75, 26);
 			this.butCancel.TabIndex = 4;
@@ -90,11 +101,12 @@ namespace OpenDental{
 			// butOK
 			// 
 			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butOK.Autosize = true;
 			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butOK.CornerRadius = 4F;
-			this.butOK.Location = new System.Drawing.Point(520, 292);
+			this.butOK.Location = new System.Drawing.Point(478, 316);
 			this.butOK.Name = "butOK";
 			this.butOK.Size = new System.Drawing.Size(75, 26);
 			this.butOK.TabIndex = 3;
@@ -103,20 +115,51 @@ namespace OpenDental{
 			// 
 			// checkMedical
 			// 
-			this.checkMedical.Location = new System.Drawing.Point(32, 294);
+			this.checkMedical.Location = new System.Drawing.Point(32, 194);
 			this.checkMedical.Name = "checkMedical";
-			this.checkMedical.Size = new System.Drawing.Size(227, 24);
+			this.checkMedical.Size = new System.Drawing.Size(217, 24);
 			this.checkMedical.TabIndex = 11;
 			this.checkMedical.Text = "Include Medical Procedures";
 			this.checkMedical.UseVisualStyleBackColor = true;
 			this.checkMedical.Visible = false;
+			// 
+			// checkAllClin
+			// 
+			this.checkAllClin.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.checkAllClin.Location = new System.Drawing.Point(289, 212);
+			this.checkAllClin.Name = "checkAllClin";
+			this.checkAllClin.Size = new System.Drawing.Size(95, 16);
+			this.checkAllClin.TabIndex = 60;
+			this.checkAllClin.Text = "All";
+			this.checkAllClin.Click += new System.EventHandler(this.checkAllClin_Click);
+			// 
+			// listClin
+			// 
+			this.listClin.Location = new System.Drawing.Point(289, 231);
+			this.listClin.Name = "listClin";
+			this.listClin.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.listClin.Size = new System.Drawing.Size(154, 147);
+			this.listClin.TabIndex = 59;
+			this.listClin.Click += new System.EventHandler(this.listClin_Click);
+			// 
+			// labelClin
+			// 
+			this.labelClin.Location = new System.Drawing.Point(286, 194);
+			this.labelClin.Name = "labelClin";
+			this.labelClin.Size = new System.Drawing.Size(104, 16);
+			this.labelClin.TabIndex = 58;
+			this.labelClin.Text = "Clinics";
+			this.labelClin.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
 			// 
 			// FormRpProcNotBilledIns
 			// 
 			this.AcceptButton = this.butOK;
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
 			this.CancelButton = this.butCancel;
-			this.ClientSize = new System.Drawing.Size(616, 366);
+			this.ClientSize = new System.Drawing.Size(565, 391);
+			this.Controls.Add(this.checkAllClin);
+			this.Controls.Add(this.listClin);
+			this.Controls.Add(this.labelClin);
 			this.Controls.Add(this.checkMedical);
 			this.Controls.Add(this.butCancel);
 			this.Controls.Add(this.butOK);
@@ -141,6 +184,46 @@ namespace OpenDental{
 			if(PrefC.GetBool(PrefName.ShowFeatureMedicalInsurance)) {
 				checkMedical.Visible=true;
 			}
+			if(PrefC.HasClinicsEnabled) {
+				_listClinics=Clinics.GetForUserod(Security.CurUser);
+				if(!Security.CurUser.ClinicIsRestricted) {
+					listClin.Items.Add(Lan.g(this,"Unassigned"));
+					listClin.SetSelected(0,true);
+				}
+				for(int i=0;i<_listClinics.Count;i++) {
+					int curIndex=listClin.Items.Add(_listClinics[i].Description);
+					if(FormOpenDental.ClinicNum==0) {
+						listClin.SetSelected(curIndex,true);
+						checkAllClin.Checked=true;
+					}
+					if(_listClinics[i].ClinicNum==FormOpenDental.ClinicNum) {
+						listClin.SelectedIndices.Clear();
+						listClin.SetSelected(curIndex,true);
+					}
+				}
+			}
+			else {
+				listClin.Visible=false;
+				labelClin.Visible=false;
+				checkAllClin.Visible=false;
+			}
+		}
+
+		private void checkAllClin_Click(object sender,EventArgs e) {
+			if(checkAllClin.Checked) {
+				for(int i=0;i<listClin.Items.Count;i++) {
+					listClin.SetSelected(i,true);
+				}
+			}
+			else {
+				listClin.SelectedIndices.Clear();
+			}
+		}
+
+		private void listClin_Click(object sender,EventArgs e) {
+			if(listClin.SelectedIndices.Count>0) {
+				checkAllClin.Checked=false;
+			}
 		}
 
 		private void butOK_Click(object sender,System.EventArgs e) {
@@ -148,57 +231,82 @@ namespace OpenDental{
 				MsgBox.Show(this,"End date cannot be before start date.");
 				return;
 			}
-			ReportSimpleGrid report=new ReportSimpleGrid();
-			//if(radioRange.Checked){
-			report.Query="SELECT ";
-			if(PrefC.GetBool(PrefName.ReportsShowPatNum)) {
-				report.Query+=DbHelper.Concat("CAST(patient.PatNum AS CHAR)","'-'","patient.LName","', '","patient.FName","' '","patient.MiddleI");
+			if(PrefC.HasClinicsEnabled) {
+				if(!checkAllClin.Checked && listClin.SelectedIndices.Count==0) {
+					MsgBox.Show(this,"At least one clinic must be selected.");
+					return;
+				}
+			}
+			List<long> listClinicNums=new List<long>();
+			if(PrefC.HasClinicsEnabled) {
+				for(int i=0;i<listClin.SelectedIndices.Count;i++) {
+					if(Security.CurUser.ClinicIsRestricted) {
+						listClinicNums.Add(_listClinics[listClin.SelectedIndices[i]].ClinicNum);//we know that the list is a 1:1 to _listClinics
+					}
+					else {
+						if(listClin.SelectedIndices[i]==0) {
+							listClinicNums.Add(0);
+						}
+						else {
+							listClinicNums.Add(_listClinics[listClin.SelectedIndices[i]-1].ClinicNum);//Minus 1 from the selected index
+						}
+					}
+				}
+			}
+			DataTable tableNotBilled=RpProcNotBilledIns.GetProcsNotBilled(listClinicNums,checkMedical.Checked,date1.SelectionStart,date2.SelectionStart);
+			string subtitleClinics="";
+			if(PrefC.HasClinicsEnabled) {
+				if(checkAllClin.Checked) {
+					subtitleClinics=Lan.g(this,"All Clinics");
+				}
+				else {
+					for(int i=0;i<listClin.SelectedIndices.Count;i++) {
+						if(i>0) {
+							subtitleClinics+=", ";
+						}
+						if(Security.CurUser.ClinicIsRestricted) {
+							subtitleClinics+=_listClinics[listClin.SelectedIndices[i]].Description;
+						}
+						else {
+							if(listClin.SelectedIndices[i]==0) {
+								subtitleClinics+=Lan.g(this,"Unassigned");
+							}
+							else {
+								subtitleClinics+=_listClinics[listClin.SelectedIndices[i]-1].Description;//Minus 1 from the selected index
+							}
+						}
+					}
+				}
+			}
+			ReportComplex report=new ReportComplex(true,false);
+			report.ReportName=Lan.g(this,"Procedures Not Billed to Insurance");
+			report.AddTitle("Title",Lan.g(this,"Procedures Not Billed to Insurance"));
+			report.AddSubTitle("Practice Name",PrefC.GetString(PrefName.PracticeTitle));
+			if(date1.SelectionStart==date2.SelectionStart) {
+				report.AddSubTitle("Report Dates",date1.SelectionStart.ToShortDateString());
 			}
 			else {
-				report.Query+=DbHelper.Concat("patient.LName","', '","patient.FName","' '","patient.MiddleI");
+				report.AddSubTitle("Report Dates",date1.SelectionStart.ToShortDateString()+" - "+date2.SelectionStart.ToShortDateString());
 			}
-			report.Query+=" AS 'PatientName',procedurelog.ProcDate,procedurecode.Descript,procedurelog.ProcFee "
-				+"FROM patient,procedurecode,procedurelog,claimproc,insplan "
-				+"WHERE claimproc.procnum=procedurelog.procnum "
-				+"AND patient.PatNum=procedurelog.PatNum "
-				+"AND procedurelog.CodeNum=procedurecode.CodeNum "
-				+"AND claimproc.PlanNum=insplan.PlanNum ";
-			if(!checkMedical.Checked) {
-				//Users would have no way to see why patients are being excluded from their billing list(s) without this check box when using the 'Exclude if insurance pending' option.
-				report.Query+="AND insplan.IsMedical=0 ";
+			if(PrefC.HasClinicsEnabled) {
+				report.AddSubTitle("Clinics",subtitleClinics);
 			}
-			report.Query+="AND claimproc.NoBillIns=0 "
-				+"AND procedurelog.ProcFee>0 "
-				+"AND claimproc.Status=6 "//estimate
-				+"AND procedurelog.procstatus=2 "
-				+"AND procedurelog.ProcDate >= "+POut.Date(date1.SelectionStart)+" "
-				+"AND procedurelog.ProcDate <= "+POut.Date(date2.SelectionStart)+" "
-				+"GROUP BY procedurelog.ProcNum "
-				+"ORDER BY patient.LName,patient.FName";
-			/*}
-			else{
-				report.Query="SELECT CONCAT(patient.LName,', ',patient.FName,' ',patient.MiddleI),"
-					+"procedurelog.ProcDate,procedurecode.Descript,procedurelog.ProcFee FROM patient,procedurecode,"
-					+"procedurelog LEFT JOIN claimproc ON claimproc.procnum = procedurelog.procnum "
-					+"WHERE claimproc.procnum IS NULL "
-					+"&& patient.patnum=procedurelog.patnum && procedurelog.codenum=procedurecode.codenum "
-					+"&& patient.priplannum > 0 "
-					+"&& procedurelog.nobillins = 0 && procedurelog.procstatus = 2 "
-					+"&& procedurelog.ProcDate = '" + date1.SelectionStart.ToString("yyyy-MM-dd")+"'";
-			}*/
-			FormQuery2=new FormQuery(report);
-			FormQuery2.IsReport=true;
-			FormQuery2.SubmitReportQuery();
-			report.Title="Procedures Not Billed to Insurance";
-			report.SubTitle.Add(PrefC.GetString(PrefName.PracticeTitle));
-			report.SubTitle.Add(date1.SelectionStart.ToString("d")+" - "+date2.SelectionStart.ToString("d"));
-			report.SetColumn(this,0,"Patient Name",185);
-			report.SetColumn(this,1,"Procedure Date",185);
-			report.SetColumn(this,2,"Procedure Description",185);
-			report.SetColumn(this,3,"Procedure Amount",185,HorizontalAlignment.Right);
-			FormQuery2.ShowDialog();
+			QueryObject query=report.AddQuery(tableNotBilled,DateTimeOD.Today.ToShortDateString());
+			query.AddColumn("Patient Name",200,FieldValueType.String);
+			query.AddColumn("Procedure Date",110,FieldValueType.Date);
+			query.GetColumnDetail("Procedure Date").StringFormat="d";
+			query.AddColumn("Procedure Description",300,FieldValueType.String);
+			query.AddColumn("Amount",90,FieldValueType.Number);
+			report.AddPageNum();
+			if(!report.SubmitQueries()) {
+				return;
+			}
+			FormReportComplex FormR=new FormReportComplex(report);
+			FormR.ShowDialog();
 			DialogResult=DialogResult.OK;
 		}
+
+
 		
 	}
 }
