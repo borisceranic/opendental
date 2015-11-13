fb8376137e4445273b2cc87c3dc33898e9c315b1
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-11-13 00:09:29 +0000

    Account Module, quick add procedures now validates procedures that require teeth, surfaces, or quadrants. Backported to: 15.4.1.0 Reviewed by: Cameron
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13245 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormDefEdit.cs
+++ OpenDental/Forms/FormDefEdit.cs
@@ -471,10 +471,6 @@ namespace OpenDental{
 							MessageBox.Show(Lan.g(this,"Invalid procedure code entered")+": "+procCodes[i].Trim());
 							return;
 						}
-						if(procCode.TreatArea!=TreatmentArea.None || procCode.TreatArea!=TreatmentArea.Mouth) {
-							MsgBox.Show(this,"You can only use a procedure code that has type None or Mouth.");
-							return;
-						}
 						listProcCodes.Add(procCodes[i].Trim());
 					}
 					textValue.Text=String.Join(",",listProcCodes);
