ecfa8a8e13b8c63c347971bce6548d2269ba0a45
joe <joe@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-12-01 16:34:15 +0000

    Move subscribes forms group boxes were in the wrong positions Backported to: 15.4.5.0 Reviewed by:
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.4@13344 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormSubscriberMove .designer.cs
+++ OpenDental/Forms/FormSubscriberMove .designer.cs
@@ -49,7 +49,7 @@ namespace OpenDental{
 			this.groupBox1.Controls.Add(this.butPickInsPlanInto);
 			this.groupBox1.Controls.Add(this.textCarrierNameInto);
 			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Location = new System.Drawing.Point(9, 88);
+			this.groupBox1.Location = new System.Drawing.Point(9, 32);
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.Size = new System.Drawing.Size(478, 50);
 			this.groupBox1.TabIndex = 2;
@@ -109,7 +109,7 @@ namespace OpenDental{
 			this.groupBox2.Controls.Add(this.butViewInsPlanFrom);
 			this.groupBox2.Controls.Add(this.butPickInsPlanFrom);
 			this.groupBox2.Controls.Add(this.textCarrierNameFrom);
-			this.groupBox2.Location = new System.Drawing.Point(9, 32);
+			this.groupBox2.Location = new System.Drawing.Point(9, 88);
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.Size = new System.Drawing.Size(478, 50);
 			this.groupBox2.TabIndex = 1;
