2329c9f217eab2f7db0bf6db5cba19d30b08b7d8
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-12-09 17:21:41 +0000

    Cache, refreshing the entire cache would sometimes cause an error. Backported to: 15.4.8.0 Reviewed by: Jason *Backport of a change I made to the head.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13387 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Cache/Cache.cs
+++ OpenDentBusiness/Cache/Cache.cs
@@ -292,6 +292,12 @@ namespace OpenDentBusiness {
 			if(itypes.Contains((int)InvalidType.AllLocal)) {
 				isAll=true;
 			}
+			//All Internal OD Tables that are cached go here
+			if(PrefC.IsODHQ) {
+				if(itypes.Contains((int)InvalidType.JobRoles) || isAll) {
+					ds.Tables.Add(JobRoles.RefreshCache());
+				}
+			}
 			if(itypes.Contains((int)InvalidType.AccountingAutoPays) || isAll) {
 				AccountingAutoPays.FillCache(ds.Tables["AccountingAutoPay"]);
 			}
@@ -377,9 +383,6 @@ namespace OpenDentBusiness {
 			if(itypes.Contains((int)InvalidType.InsFilingCodes) || isAll){
 				InsFilingCodes.FillCache(ds.Tables["InsFilingCode"]);
 				InsFilingCodeSubtypes.FillCache(ds.Tables["InsFilingCodeSubtype"]);
-			}
-			if(itypes.Contains((int)InvalidType.JobRoles) || isAll) {
-				JobRoles.FillCache(ds.Tables["JobRole"]);
 			}
 			if(itypes.Contains((int)InvalidType.Languages) || isAll) {
 				Lans.FillCache(ds.Tables["Language"]);
