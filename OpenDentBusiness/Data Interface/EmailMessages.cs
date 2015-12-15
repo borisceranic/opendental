730ab84d155f79ee7a5edadc8341aa8b07f7eb0a
josh <josh@098d0599-7f2b-0410-95a5-ad30ac85393a>
2015-12-15 21:23:37 +0000

    Patient Portal, pressing transmitting a direct message CCD would sometimes throw an exception. Backported to: 15.4.9.0 Reviewed by:  Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13426 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/EmailMessages.cs
+++ OpenDentBusiness/Data Interface/EmailMessages.cs
@@ -421,7 +421,8 @@ namespace OpenDentBusiness{
 		private static void WireEmailUnsecure(Health.Direct.Agent.OutgoingMessage msgOut,EmailAddress emailAddress,long patNum) {
 			//No need to check RemotingRole; no call to db.
 			//When batch email operations are performed, we sometimes do this check further up in the UI.  This check is here to as a catch-all.
-			if(!Security.IsAuthorized(Permissions.EmailSend,DateTime.Now,true,Security.CurUser.UserGroupNum)) {//This overload throws an exception if user is not authorized.
+            //Security.CurUser will be null if this is called from a third party application (like Patient Portal).  We want to continue if that is the case.
+			if(Security.CurUser!=null && !Security.IsAuthorized(Permissions.EmailSend,DateTime.Now,true,Security.CurUser.UserGroupNum)) {//This overload throws an exception if user is not authorized.
 				return;
 			}
 			if(emailAddress.IsImplicitSsl) {
@@ -506,7 +507,8 @@ namespace OpenDentBusiness{
 		private static void WireEmailUnsecure(EmailMessage emailMessage,EmailAddress emailAddress,NameValueCollection nameValueCollectionHeaders,params AlternateView[] arrayAlternateViews) {
 			//No need to check RemotingRole; no call to db.
 			//When batch email operations are performed, we sometimes do this check further up in the UI.  This check is here to as a catch-all.
-			if(!Security.IsAuthorized(Permissions.EmailSend,DateTime.Now,true,Security.CurUser.UserGroupNum)){//This overload throws an exception if user is not authorized.
+            //Security.CurUser will be null if this is called from a third party application (like Patient Portal).  We want to continue if that is the case.
+			if(Security.CurUser!=null && !Security.IsAuthorized(Permissions.EmailSend,DateTime.Now,true,Security.CurUser.UserGroupNum)){//This overload throws an exception if user is not authorized.
 				return;
 			}
 			if(emailAddress.ServerPort==465) {//implicit
