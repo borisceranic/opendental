9fb7a1f6841511413cbfb2ad90cc66f5f4d62b03
joe <joe@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-02-02 21:45:15 +0000

    UserControlTasks tabs would sometimes cause a UE when clicking down and dragging off of tab Back-ported to: 15.4 Reviewed By: Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13707 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/UserControlTasks.cs
+++ OpenDental/User Controls/UserControlTasks.cs
@@ -611,8 +611,8 @@ namespace OpenDental {
 			}
 			TaskNoteList=TaskNotes.RefreshForTasks(taskNums);
 		}
-
-		private void tabContr_Click(object sender,System.EventArgs e) {
+		
+		private void tabContr_MouseDown(object sender,MouseEventArgs e) {
 			TreeHistory=new List<TaskList>();//clear the tree no matter which tab clicked.
 			FillTree();
 			FillGrid();
