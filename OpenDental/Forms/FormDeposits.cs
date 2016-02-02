eb3feced3128311d00a1c155667844c368946c95
andrew <andrew@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-02-02 21:42:38 +0000

    Deposits form is now organised and resizes more intuitively. Backported to: 15.4.25.0 Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@13706 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormDeposits.cs
+++ OpenDental/Forms/FormDeposits.cs
@@ -66,26 +66,30 @@ namespace OpenDental{
 			// 
 			// butClose
 			// 
-			this.butClose.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butClose.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butClose.Autosize = true;
 			this.butClose.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butClose.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butClose.CornerRadius = 4F;
-			this.butClose.Location = new System.Drawing.Point(382,576);
+			this.butClose.Location = new System.Drawing.Point(215, 574);
 			this.butClose.Name = "butClose";
-			this.butClose.Size = new System.Drawing.Size(75,26);
-			this.butClose.TabIndex = 0;
+			this.butClose.Size = new System.Drawing.Size(75, 26);
+			this.butClose.TabIndex = 3;
 			this.butClose.Text = "&Close";
 			this.butClose.Click += new System.EventHandler(this.butClose_Click);
 			// 
 			// grid
 			// 
+			this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.grid.HasAddButton = false;
+			this.grid.HasMultilineHeaders = false;
 			this.grid.HScrollVisible = false;
-			this.grid.Location = new System.Drawing.Point(18,13);
+			this.grid.Location = new System.Drawing.Point(18, 13);
 			this.grid.Name = "grid";
 			this.grid.ScrollValue = 0;
-			this.grid.Size = new System.Drawing.Size(189,588);
+			this.grid.Size = new System.Drawing.Size(189, 588);
 			this.grid.TabIndex = 1;
 			this.grid.Title = "Deposit Slips";
 			this.grid.TranslationName = "TableDepositSlips";
@@ -93,48 +97,50 @@ namespace OpenDental{
 			// 
 			// butAdd
 			// 
-			this.butAdd.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butAdd.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butAdd.Anchor = System.Windows.Forms.AnchorStyles.Right;
 			this.butAdd.Autosize = true;
 			this.butAdd.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butAdd.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butAdd.CornerRadius = 4F;
 			this.butAdd.Image = global::OpenDental.Properties.Resources.Add;
 			this.butAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butAdd.Location = new System.Drawing.Point(223,576);
+			this.butAdd.Location = new System.Drawing.Point(213, 293);
 			this.butAdd.Name = "butAdd";
-			this.butAdd.Size = new System.Drawing.Size(77,26);
-			this.butAdd.TabIndex = 98;
+			this.butAdd.Size = new System.Drawing.Size(77, 26);
+			this.butAdd.TabIndex = 0;
 			this.butAdd.Text = "Add";
 			this.butAdd.Click += new System.EventHandler(this.butAdd_Click);
 			// 
 			// butOK
 			// 
-			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butOK.Autosize = true;
 			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butOK.CornerRadius = 4F;
-			this.butOK.Location = new System.Drawing.Point(382,534);
+			this.butOK.Location = new System.Drawing.Point(215, 542);
 			this.butOK.Name = "butOK";
-			this.butOK.Size = new System.Drawing.Size(75,26);
-			this.butOK.TabIndex = 99;
+			this.butOK.Size = new System.Drawing.Size(75, 26);
+			this.butOK.TabIndex = 2;
 			this.butOK.Text = "&OK";
 			this.butOK.Click += new System.EventHandler(this.butOK_Click);
 			// 
 			// FormDeposits
 			// 
 			this.AcceptButton = this.butOK;
-			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
-			this.ClientSize = new System.Drawing.Size(486,613);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(304, 613);
 			this.Controls.Add(this.butOK);
 			this.Controls.Add(this.grid);
 			this.Controls.Add(this.butClose);
 			this.Controls.Add(this.butAdd);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(320, 1200);
 			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(320, 250);
 			this.Name = "FormDeposits";
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
