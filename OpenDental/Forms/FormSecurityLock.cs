f8f6e63cd4baa73639aa667753641c9b914df560
ryan <ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-02-23 00:15:42 +0000

    Security, lock days enhanced to prevent storing invalid number of days. Backported to: 15.4.33.0;16.1.1.0 Reviewed by:Allen
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.4@13883 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormSecurityLock.cs
+++ OpenDental/Forms/FormSecurityLock.cs
@@ -62,106 +62,106 @@ namespace OpenDental{
 			this.textDays = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			this.textDate = new OpenDental.ValidDate();
 			this.butOK = new OpenDental.UI.Button();
 			this.butCancel = new OpenDental.UI.Button();
-			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(12,9);
+			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(445,135);
+			this.label1.Size = new System.Drawing.Size(445, 135);
 			this.label1.TabIndex = 2;
 			this.label1.Text = resources.GetString("label1.Text");
 			// 
 			// checkAdmin
 			// 
 			this.checkAdmin.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.checkAdmin.Location = new System.Drawing.Point(15,203);
+			this.checkAdmin.Location = new System.Drawing.Point(15, 203);
 			this.checkAdmin.Name = "checkAdmin";
-			this.checkAdmin.Size = new System.Drawing.Size(224,16);
+			this.checkAdmin.Size = new System.Drawing.Size(224, 16);
 			this.checkAdmin.TabIndex = 58;
 			this.checkAdmin.Text = "Lock includes administrators";
 			// 
 			// textDays
 			// 
-			this.textDays.Location = new System.Drawing.Point(88,173);
+			this.textDays.Location = new System.Drawing.Point(88, 173);
 			this.textDays.Name = "textDays";
-			this.textDays.Size = new System.Drawing.Size(46,20);
+			this.textDays.Size = new System.Drawing.Size(46, 20);
 			this.textDays.TabIndex = 59;
 			this.textDays.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textDays_KeyDown);
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(21,147);
+			this.label3.Location = new System.Drawing.Point(21, 147);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(65,18);
+			this.label3.Size = new System.Drawing.Size(65, 18);
 			this.label3.TabIndex = 60;
 			this.label3.Text = "Date";
 			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// label4
 			// 
-			this.label4.Location = new System.Drawing.Point(17,174);
+			this.label4.Location = new System.Drawing.Point(17, 174);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(70,16);
+			this.label4.Size = new System.Drawing.Size(70, 16);
 			this.label4.TabIndex = 61;
 			this.label4.Text = "Days";
 			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(139, 175);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(234, 16);
+			this.label2.TabIndex = 63;
+			this.label2.Text = "1 means only today, 0 to disable.";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
 			// textDate
 			// 
-			this.textDate.Location = new System.Drawing.Point(88,147);
+			this.textDate.Location = new System.Drawing.Point(88, 147);
 			this.textDate.Name = "textDate";
-			this.textDate.Size = new System.Drawing.Size(100,20);
+			this.textDate.Size = new System.Drawing.Size(100, 20);
 			this.textDate.TabIndex = 62;
 			this.textDate.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textDate_KeyDown);
 			// 
 			// butOK
 			// 
-			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butOK.Autosize = true;
 			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butOK.CornerRadius = 4F;
-			this.butOK.Location = new System.Drawing.Point(298,239);
+			this.butOK.Location = new System.Drawing.Point(298, 239);
 			this.butOK.Name = "butOK";
-			this.butOK.Size = new System.Drawing.Size(75,24);
+			this.butOK.Size = new System.Drawing.Size(75, 24);
 			this.butOK.TabIndex = 1;
 			this.butOK.Text = "&OK";
 			this.butOK.Click += new System.EventHandler(this.butOK_Click);
 			// 
 			// butCancel
 			// 
-			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.butCancel.Autosize = true;
 			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butCancel.CornerRadius = 4F;
-			this.butCancel.Location = new System.Drawing.Point(379,239);
+			this.butCancel.Location = new System.Drawing.Point(379, 239);
 			this.butCancel.Name = "butCancel";
-			this.butCancel.Size = new System.Drawing.Size(75,24);
+			this.butCancel.Size = new System.Drawing.Size(75, 24);
 			this.butCancel.TabIndex = 0;
 			this.butCancel.Text = "&Cancel";
 			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
 			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(139,175);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(123,16);
-			this.label2.TabIndex = 63;
-			this.label2.Text = "1 means only today";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			// 
 			// FormSecurityLock
 			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
-			this.ClientSize = new System.Drawing.Size(470,279);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(470, 279);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.textDate);
 			this.Controls.Add(this.textDays);
@@ -209,7 +209,26 @@ namespace OpenDental{
 				MsgBox.Show(this,"Please fix error first.");
 				return;
 			}
-			int days=PIn.Int(textDays.Text);
+			int days = 0;
+			if(!int.TryParse(textDays.Text,out days) && !string.IsNullOrEmpty(textDays.Text)) {
+				MsgBox.Show(this,"Invalid number of days.");
+				return;
+			}
+			if(days<0) {
+				days=0;
+			}
+			if(days>3650) {
+				switch(MessageBox.Show("Lock days set to greater than ten years, would you like to disable lock days instead?","",MessageBoxButtons.YesNoCancel)) {
+					case DialogResult.Cancel:
+						return;
+					case DialogResult.OK:
+					case DialogResult.Yes:
+						days=0;//disable instead of using large value.
+						break;
+					default:
+						break;
+				}
+			}
 			DateTime date=PIn.Date(textDate.Text);
 			if(Prefs.UpdateString(PrefName.SecurityLockDate,POut.Date(date,false))
 				| Prefs.UpdateInt(PrefName.SecurityLockDays,days)
