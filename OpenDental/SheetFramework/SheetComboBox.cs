aafe0b2f19ba6ba5a416352741cb4e1cbc368eb4
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-03-02 00:14:46 +0000

    Sheet combo boxes weren't displaying the correct default values sometimes. Backported to: 16.1.6.0 Reviewed by: Jason
    - Also improved readability of the code quite a bit.
    
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental16.1@13961 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/SheetFramework/SheetComboBox.cs
+++ OpenDental/SheetFramework/SheetComboBox.cs
@@ -9,12 +9,12 @@ using System.Windows.Forms;
 
 namespace OpenDental {
 	public partial class SheetComboBox:Control {
-		private Pen pen;
-		private bool isHovering;
-		private PathGradientBrush hoverBrush;
-		private Color surroundColor;
+		private bool _isHovering;
+		private PathGradientBrush _hoverBrush;
+		private Color _surroundColor;
 		public string SelectedOption;
 		private string[] _arrayComboOptions;
+		///<summary>A default option for the combo box to display.  Required to be set for screening tooth chart combo boxes.  E.g. "ling", "b".</summary>
 		public string DefaultOption;
 		public bool IsToothChart;
 		private ContextMenu _contextMenu=new ContextMenu();
@@ -28,20 +28,8 @@ namespace OpenDental {
 			}
 		}
 
-		public SheetComboBox() {
-			InitializeComponent();
-			//_selectedOption="";
-			_arrayComboOptions=new string[7];
-			_arrayComboOptions[0]="None";
-			_arrayComboOptions[1]="S";
-			_arrayComboOptions[2]="PS";
-			_arrayComboOptions[3]="C";
-			_arrayComboOptions[4]="F";
-			_arrayComboOptions[5]="NFE";
-			_arrayComboOptions[6]="NN";
-			foreach(string option in _arrayComboOptions) {
-				_contextMenu.MenuItems.Add(new MenuItem(option,menuItemContext_Click));
-			}
+		///<summary>Currently fills the combo box with the default options for combo boxes on screen charts.</summary>
+		public SheetComboBox() : this(";None|S|PS|C|F|NFE|NN") {
 		}
 
 		public SheetComboBox(string values) {
@@ -64,74 +52,63 @@ namespace OpenDental {
 			SelectedOption=_arrayComboOptions[_contextMenu.MenuItems.IndexOf((MenuItem)sender)];
 		}
 
-		protected override void OnSizeChanged(EventArgs e) {
-			base.OnSizeChanged(e);
-			SetBrushes();
-		}
-
 		private void SheetComboBox_MouseDown(object sender,MouseEventArgs e) {
 			_contextMenu.Show(this,new Point(0,Height));//Can't resize width, it's done according to width of items.
 		}
 
 		private void SetBrushes(){
-			pen=new Pen(Color.Black,1.6f);
-			hoverBrush=new PathGradientBrush(
+			_hoverBrush=new PathGradientBrush(
 				new Point[] {new Point(0,0),new Point(Width-1,0),new Point(Width-1,Height-1),new Point(0,Height-1)});
-			hoverBrush.CenterColor=Color.White;
-			surroundColor=Color.FromArgb(245,234,200);
-			hoverBrush.SurroundColors=new Color[] {surroundColor,surroundColor,surroundColor,surroundColor};
+			_hoverBrush.CenterColor=Color.White;
+			_surroundColor=Color.FromArgb(245,234,200);
+			_hoverBrush.SurroundColors=new Color[] {_surroundColor,_surroundColor,_surroundColor,_surroundColor};
 			Blend blend=new Blend();
 			float[] myFactors = {0f,.5f,1f,1f,1f,1f};
 			float[] myPositions = {0f,.2f,.4f,.6f,.8f,1f};
 			blend.Factors=myFactors;
 			blend.Positions=myPositions;
-			hoverBrush.Blend=blend;
+			_hoverBrush.Blend=blend;
 		}
 
 		protected override void OnPaint(PaintEventArgs pe) {
 			base.OnPaint(pe);
-			Graphics g=pe.Graphics;
-			g.SmoothingMode=SmoothingMode.HighQuality;
-			g.CompositingQuality=CompositingQuality.HighQuality;
-			g.FillRectangle(Brushes.White,0,0,Width,Height);//White background
-			if(isHovering){
-				g.FillRectangle(hoverBrush,0,0,Width-1,Height-1);
-				g.DrawRectangle(new Pen(surroundColor),0,0,Width-1,Height-1);
+			using(Graphics g=pe.Graphics) {
+				g.SmoothingMode=SmoothingMode.HighQuality;
+				g.CompositingQuality=CompositingQuality.HighQuality;
+				g.FillRectangle(Brushes.White,0,0,Width,Height);//White background
+				if(_isHovering) {
+					g.FillRectangle(_hoverBrush,0,0,Width-1,Height-1);
+					g.DrawRectangle(new Pen(_surroundColor),0,0,Width-1,Height-1);
+				}
+				g.DrawRectangle(Pens.Black,-1,-1,Width,Height);//Outline
+				StringFormat stringFormat=new StringFormat();
+				stringFormat.Alignment=StringAlignment.Center;
+				stringFormat.LineAlignment=StringAlignment.Center;
+				Brush brush=Brushes.Black;//Default to black.  Do not dispose brush because that will dispose of Brushes.Black... not good.
+				if(ToothChart) {
+					if(SelectedOption=="buc" || SelectedOption=="ling" || SelectedOption=="d" || SelectedOption=="m" || SelectedOption=="None") {
+						brush=Brushes.LightGray;
+					}
+					if(SelectedOption=="None") {
+						SelectedOption=DefaultOption;//Nothing has been selected so draw the "default" string in the combo box.  E.g. "b", "ling", etc.
+					}
+				}
+				g.DrawString(SelectedOption,new Font(FontFamily.GenericSansSerif,IsToothChart ? 10f : this.Height-10)
+					,brush,new Point(this.Width/2,this.Height/2),stringFormat);
 			}
-			g.DrawRectangle(Pens.Black,-1,-1,Width,Height);//Outline
-			//g.DrawRectangle(Pens.Black,Width-3,0,this.Width-3,this.Height-1);//Outline
-			StringFormat stringFormat=new StringFormat();
-			stringFormat.Alignment=StringAlignment.Center;
-			stringFormat.LineAlignment=StringAlignment.Center;
-			if(SelectedOption!="buc" && SelectedOption!="ling" && SelectedOption!="d" && SelectedOption!="m" && SelectedOption!="Clear") {
-				g.DrawString(SelectedOption,new Font(FontFamily.GenericSansSerif,IsToothChart ? 10f:this.Height-10),Brushes.Black,new Point(this.Width/2,this.Height/2),stringFormat);//Draws selected option in box, centering it on the point supplied.
-			}
-			else if(SelectedOption=="None") {
-				SelectedOption=DefaultOption;
-				g.DrawString(DefaultOption,new Font(FontFamily.GenericSansSerif,IsToothChart ? 10f:this.Height-10),Brushes.LightGray,new Point(this.Width/2,this.Height/2),stringFormat);//Draws default option in box for D3 sheets
-			}
-			else {
-				g.DrawString(DefaultOption,new Font(FontFamily.GenericSansSerif,IsToothChart ? 10f:this.Height-10),Brushes.LightGray,new Point(this.Width/2,this.Height/2),stringFormat);//Draws default option in box for D3 sheets
-			}
-			g.Dispose();
-		}
-
-		protected override void OnMouseDown(MouseEventArgs e) {
-			base.OnMouseDown(e);
-			
 		}
 
 		protected override void OnMouseMove(MouseEventArgs e) {
 			base.OnMouseMove(e);
-			if(!isHovering) {
-				isHovering=true;
+			if(!_isHovering) {
+				_isHovering=true;
 				Invalidate();
 			}
 		}
 
 		protected override void OnMouseLeave(EventArgs e) {
 			base.OnMouseLeave(e);
-			isHovering=false;
+			_isHovering=false;
 			Invalidate();
 		}
 
