0f8fd249936179f1db6919d04cc358180390bfcd
chris <chris@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-03-04 20:32:53 +0000

    When signing TPs, the Pat column would not display if Insurance Estimates was unchecked and Discounts was checked. Reviewed by: Ryan Backported to: 15.4.37 and 16.1.7
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental15.4@13987 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/ContrTreat.cs
+++ OpenDental/Main Modules/ContrTreat.cs
@@ -1449,7 +1449,7 @@ namespace OpenDental{
 				if(fields[i].InternalName=="Discount" && !checkShowDiscount.Checked) {
 					continue;
 				}
-				if(fields[i].InternalName=="Pat" && !checkShowIns.Checked) {
+				if(fields[i].InternalName=="Pat" && !checkShowIns.Checked && !checkShowDiscount.Checked) {
 					continue;
 				}
 				if(fields[i].InternalName=="Fee" 
@@ -1580,7 +1580,7 @@ namespace OpenDental{
 							}
 							break;
 						case "Pat":
-							if(checkShowIns.Checked) {
+							if(checkShowIns.Checked || checkShowDiscount.Checked) {
 								if(PrefC.GetBool(PrefName.TreatPlanItemized) || RowsMain[i].Description.ToString()=="Total"
 									|| RowsMain[i].Description.ToString()=="Subtotal")
 								{
