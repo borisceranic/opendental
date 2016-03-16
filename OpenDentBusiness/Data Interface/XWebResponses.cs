61f6dadc742a204ea383da96b6d2f8827448c1b8
sam <sam@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-03-16 23:21:08 +0000

    - Added XWebResponse table type and crud classes. Used for online payments integration. Still haven't added convert db script. Will add after fully testing and vetting all columns. Reviewed By: Cameron
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14071 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/Data Interface/XWebResponses.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace OpenDentBusiness{
+	///<summary></summary>
+	public class XWebResponses{
+		///<summary>Gets one XWebResponse from the db.</summary>
+		public static XWebResponse GetOne(long xWebResponseNum) {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				return Meth.GetObject<XWebResponse>(MethodBase.GetCurrentMethod(),xWebResponseNum);
+			}
+			return Crud.XWebResponseCrud.SelectOne(xWebResponseNum);
+		}
+
+		///<summary>Gets all XWebResponses where TransactionStatus==XWebTransactionStatus.HpfPending from the db.</summary>
+		public static List<XWebResponse> GetPendingHPFs() {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				return Meth.GetObject<List<XWebResponse>>(MethodBase.GetCurrentMethod());
+			}
+			return Crud.XWebResponseCrud.SelectMany("SELECT * FROM xwebresponse "
+				+"WHERE TransactionStatus = "+POut.Int((int)XWebTransactionStatus.HpfPending));
+		}
+
+		//If this table type will exist as cached data, uncomment the CachePattern region below and edit.
+		/*
+		#region CachePattern
+		//This region can be eliminated if this is not a table type with cached data.
+		//If leaving this region in place, be sure to add RefreshCache and FillCache 
+		//to the Cache.cs file with all the other Cache types.
+
+		///<summary>A list of all XWebResponses.</summary>
+		private static List<XWebResponse> listt;
+
+		///<summary>A list of all XWebResponses.</summary>
+		public static List<XWebResponse> Listt{
+			get {
+				if(listt==null) {
+					RefreshCache();
+				}
+				return listt;
+			}
+			set {
+				listt=value;
+			}
+		}
+
+		///<summary></summary>
+		public static DataTable RefreshCache(){
+			//No need to check RemotingRole; Calls GetTableRemotelyIfNeeded().
+			string command="SELECT * FROM xwebresponse ORDER BY ItemOrder";//stub query probably needs to be changed
+			DataTable table=Cache.GetTableRemotelyIfNeeded(MethodBase.GetCurrentMethod(),command);
+			table.TableName="XWebResponse";
+			FillCache(table);
+			return table;
+		}
+
+		///<summary></summary>
+		public static void FillCache(DataTable table){
+			//No need to check RemotingRole; no call to db.
+			listt=Crud.XWebResponseCrud.TableToList(table);
+		}
+		#endregion
+		*/
+		/*
+		Only pull out the methods below as you need them.  Otherwise, leave them commented out.
+
+		///<summary></summary>
+		public static List<XWebResponse> Refresh(long patNum){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				return Meth.GetObject<List<XWebResponse>>(MethodBase.GetCurrentMethod(),patNum);
+			}
+			string command="SELECT * FROM xwebresponse WHERE PatNum = "+POut.Long(patNum);
+			return Crud.XWebResponseCrud.SelectMany(command);
+		}
+
+		///<summary></summary>
+		public static long Insert(XWebResponse xWebResponse){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				xWebResponse.XWebResponseNum=Meth.GetLong(MethodBase.GetCurrentMethod(),xWebResponse);
+				return xWebResponse.XWebResponseNum;
+			}
+			return Crud.XWebResponseCrud.Insert(xWebResponse);
+		}
+
+		///<summary></summary>
+		public static void Update(XWebResponse xWebResponse){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),xWebResponse);
+				return;
+			}
+			Crud.XWebResponseCrud.Update(xWebResponse);
+		}
+
+		///<summary></summary>
+		public static void Delete(long xWebResponseNum) {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),xWebResponseNum);
+				return;
+			}
+			Crud.XWebResponseCrud.Delete(xWebResponseNum);
+		}
+
+		
+
+		
+		*/
+
+
+
+	}
+}
