868c08ca06ce74dd9067c86f4926f7ea9dcb8582
david <david@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-03-23 20:13:53 +0000

    CEMT, refreshing connection status would cause the program to freeze sometimes Backported to: 15.4.42.0;16.1.13.0 Reviewed by: Allen
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental16.1@14104 098d0599-7f2b-0410-95a5-ad30ac85393a

--- CentralManager/FormCentralManager.cs
+++ CentralManager/FormCentralManager.cs
@@ -381,20 +381,25 @@ namespace CentralManager {
 
 		private void ConnectAndVerify(ODThread odThread) {
 			CentralConnection connection=(CentralConnection)odThread.Parameters[0];
-			string progVersion=(string)odThread.Parameters[1];
-			if(!CentralConnectionHelper.UpdateCentralConnection(connection,false)) {
+			try {
+				string progVersion=(string)odThread.Parameters[1];
+				if(!CentralConnectionHelper.UpdateCentralConnection(connection,false)) {
+					odThread.Tag=new object[] { connection,"OFFLINE" };//Can't connect
+					return;
+				}
+				string progVersionRemote=PrefC.GetStringNoCache(PrefName.ProgramVersion);
+				string err="";
+				if(progVersionRemote!=progVersion) {
+					err=progVersionRemote;
+				}
+				else {
+					err="OK";
+				}
+				odThread.Tag=new object[] { connection,err };
+			}
+			catch(Exception ex) {
 				odThread.Tag=new object[] { connection,"OFFLINE" };//Can't connect
-				return;
 			}
-			string progVersionRemote=PrefC.GetStringNoCache(PrefName.ProgramVersion);
-			string err="";
-			if(progVersionRemote!=progVersion) {
-				err=progVersionRemote;
-			}
-			else {
-				err="OK";
-			}
-			odThread.Tag=new object[] { connection,err };
 		}
 
 		private void butRefresh_Click(object sender,EventArgs e) {
