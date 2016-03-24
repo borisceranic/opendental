3469950f0e0030420dd093ac41a862146c35c13e
joe <joe@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-03-24 19:37:15 +0000

    ODHQ only, when the Big Phones window or Call Center Status Map window were minimized they would not be brought to front when you re-clicked their corresponding buttons. Backported to: 16.1.13.0 Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14106 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/FormOpenDental.cs
+++ OpenDental/Main Modules/FormOpenDental.cs
@@ -5160,6 +5160,9 @@ namespace OpenDental{
 				formPhoneTiles.BringToFront();
 			}
 			else {
+				if(formPhoneTiles.WindowState==FormWindowState.Minimized) {
+					formPhoneTiles.WindowState=FormWindowState.Normal;
+				}
 				formPhoneTiles.Show();
 				formPhoneTiles.BringToFront();
 			}
@@ -5168,13 +5171,14 @@ namespace OpenDental{
 		private void butMapPhones_Click(object sender,EventArgs e) {
 			if(formMapHQ==null || formMapHQ.IsDisposed) {
 				formMapHQ=new FormMapHQ();
-				formMapHQ.Show();
-				formMapHQ.BringToFront();
 			}
 			else {
-				formMapHQ.Show();
-				formMapHQ.BringToFront();
+				if(formMapHQ.WindowState==FormWindowState.Minimized) {
+					formMapHQ.WindowState=FormWindowState.Normal;
+				}
 			}
+			formMapHQ.Show();
+			formMapHQ.BringToFront();
 		}
 
 		private void butTriage_Click(object sender,EventArgs e) {
