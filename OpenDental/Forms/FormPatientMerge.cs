24314babd2f7edc2311ac84dddbcd9d3434b7e31
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-04-19 03:10:56 +0000

    Patient merge tool enhanced to allow merging patients without requiring their names and birthdates to match.  eCW users already had this ability, with a warning message.  Now all OD users will be able to merge if they choose to after a warning message about the mismatch. Head only version 16.2 Reviewed by: Ryan
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14216 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormPatientMerge.cs
+++ OpenDental/Forms/FormPatientMerge.cs
@@ -1,17 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Text;
 using System.Windows.Forms;
 using CodeBase;
-using OpenDental.UI;
 using OpenDentBusiness;
 
 namespace OpenDental {
 	public partial class FormPatientMerge:ODForm {
+		///<summary>For display purposes only.  Reloaded from the db when merge actually occurs.</summary>
+		private Patient _patTo;
+		///<summary>For display purposes only.  Reloaded from the db when merge actually occurs.</summary>
+		private Patient _patFrom;
 
 		public FormPatientMerge() {
 			InitializeComponent();
@@ -25,10 +23,10 @@ namespace OpenDental {
 			FormPatientSelect fps=new FormPatientSelect();
 			if(fps.ShowDialog()==DialogResult.OK){
 				long selectedPatNum=fps.SelectedPatNum;//to prevent warning about marshal-by-reference
-				this.textPatientIDInto.Text=selectedPatNum.ToString();
-				Patient pat=Patients.GetPat(selectedPatNum);
-				this.textPatientNameInto.Text=pat.GetNameFLFormal();
-				this.textPatToBirthdate.Text=pat.Birthdate.ToShortDateString();
+				textPatientIDInto.Text=selectedPatNum.ToString();
+				_patTo=Patients.GetPat(selectedPatNum);
+				textPatientNameInto.Text=_patTo.GetNameFLFormal();
+				textPatToBirthdate.Text=_patTo.Birthdate.ToShortDateString();
 			}
 			CheckUIState();
 		}
@@ -37,43 +35,41 @@ namespace OpenDental {
 			FormPatientSelect fps=new FormPatientSelect();
 			if(fps.ShowDialog()==DialogResult.OK) {
 				long selectedPatNum=fps.SelectedPatNum;//to prevent warning about marshal-by-reference
-				this.textPatientIDFrom.Text=selectedPatNum.ToString();
-				Patient pat=Patients.GetPat(selectedPatNum);
-				this.textPatientNameFrom.Text=pat.GetNameFLFormal();
-				this.textPatFromBirthdate.Text=pat.Birthdate.ToShortDateString();
+				textPatientIDFrom.Text=selectedPatNum.ToString();
+				_patFrom=Patients.GetPat(selectedPatNum);
+				textPatientNameFrom.Text=_patFrom.GetNameFLFormal();
+				textPatFromBirthdate.Text=_patFrom.Birthdate.ToShortDateString();
 			}
 			CheckUIState();
 		}
 
 		private void CheckUIState(){
-			this.butMerge.Enabled=(this.textPatientIDInto.Text.Trim()!="" && this.textPatientIDFrom.Text.Trim()!="");
+			butMerge.Enabled=(_patTo!=null && _patFrom!=null);
 		}
 
 		private void butMerge_Click(object sender,EventArgs e) {
-			long patTo=Convert.ToInt64(this.textPatientIDInto.Text.Trim());
-			long patFrom=Convert.ToInt64(this.textPatientIDFrom.Text.Trim());
-			if(patTo==patFrom){
+			if(_patTo.PatNum==_patFrom.PatNum) {
 				MsgBox.Show(this,"Cannot merge a patient account into itself. Please select a different patient to merge from.");
 				return;
 			}
-			Patient patientFrom=Patients.GetPat(patFrom);
-			Patient patientTo=Patients.GetPat(patTo);
-			if(patientFrom.FName.Trim().ToLower()!=patientTo.FName.Trim().ToLower() ||
-					patientFrom.LName.Trim().ToLower()!=patientTo.LName.Trim().ToLower() ||
-					patientFrom.Birthdate!=patientTo.Birthdate) 
+			string msgText="";
+			if(_patFrom.FName.Trim().ToLower()!=_patTo.FName.Trim().ToLower()
+				|| _patFrom.LName.Trim().ToLower()!=_patTo.LName.Trim().ToLower()
+				|| _patFrom.Birthdate!=_patTo.Birthdate) 
 			{//mismatch
+				msgText=Lan.g(this,"The two patients do not have the same first name, last name, and birthdate.");
 				if(Programs.UsingEcwTightOrFullMode()) {
-					if(!MsgBox.Show(this,MsgBoxButtons.YesNo,@"The two selected patients do not have the same first name, last name, and date of birth.  The patients must first be merged from within eCW, then immediately merged in the same order in Open Dental.  If the patients are not merged in this manner, some information may not properly bridge between eCW and Open Dental.
-Into patient name: "+patientTo.FName+" "+patientTo.LName+", Into patient birthdate: "+patientTo.Birthdate.ToShortDateString()+@".
-From patient name: "+patientFrom.FName+" "+patientFrom.LName+", From paient birthdate: "+patientFrom.Birthdate.ToShortDateString()+@".
-Merge the patient at the bottom into the patient shown at the top?")) 
-					{
-						return;//The user chose not to merge
-					}
+					msgText+="\r\n"+Lan.g(this,"The patients must first be merged from within eCW, then immediately merged in the same order in Open Dental.  "
+						+"If the patients are not merged in this manner, some information may not properly bridge between eCW and Open Dental.");
 				}
-				else {//not eCW
-					MsgBox.Show(this,"The two selected patients do not have the same first name, last name, and date of birth.  You must set all of those the same before merge is allowed.");
-					return;//Do not merge.
+				msgText+="\r\n\r\n"
+					+Lan.g(this,"Into patient name")+": "+Patients.GetNameFLnoPref(_patTo.LName,_patTo.FName,"")+", "//using Patients.GetNameFLnoPref to omit MiddleI
+					+Lan.g(this,"Into patient birthdate")+": "+_patTo.Birthdate.ToShortDateString()+"\r\n"
+					+Lan.g(this,"From patient name")+": "+Patients.GetNameFLnoPref(_patFrom.LName,_patFrom.FName,"")+", "//using Patients.GetNameFLnoPref to omit MiddleI
+					+Lan.g(this,"From paient birthdate")+": "+_patFrom.Birthdate.ToShortDateString()+"\r\n\r\n"
+					+Lan.g(this,"Merge the patient on the bottom into the patient shown on the top?");
+				if(MessageBox.Show(msgText,"",MessageBoxButtons.YesNo)!=DialogResult.Yes) {
+					return;//The user chose not to merge
 				}
 			}
 			else {//name and bd match
@@ -81,86 +77,84 @@ Merge the patient at the bottom into the patient shown at the top?"))
 					return;//The user chose not to merge.
 				}
 			}
-			this.Cursor=Cursors.WaitCursor;
-			if(patientFrom.PatNum==patientFrom.Guarantor){
-				Family fam=Patients.GetFamily(patFrom);
-				if(fam.ListPats.Length>1){
-					this.Cursor=Cursors.Default;
-					if(!MsgBox.Show(this,MsgBoxButtons.YesNo,
-						"The patient you have chosen to merge from is a guarantor. Merging this patient into another account will "
-						+"cause all familiy members of the patient being merged from to be moved into the same family as the "
-						+"patient account being merged into. Do you wish to continue with the merge?")) {
+			if(_patFrom.PatNum==_patFrom.Guarantor) {
+				Family fam=Patients.GetFamily(_patFrom.PatNum);
+				if(fam.ListPats.Length>1) {
+					msgText=Lan.g(this,"The patient you have chosen to merge from is a guarantor.  Merging this patient into another account will cause all "
+						+"familiy members of the patient being merged from to be moved into the same family as the patient account being merged into.")+"\r\n"
+						+Lan.g(this,"Do you wish to continue with the merge?");
+					if(MessageBox.Show(msgText,"",MessageBoxButtons.YesNo)!=DialogResult.Yes) {
 						return;//The user chose not to merge.
 					}
-					this.Cursor=Cursors.WaitCursor;
 				}
 			}
-			if(Patients.MergeTwoPatients(patTo,patFrom)) {
+			Cursor=Cursors.WaitCursor;
+			if(Patients.MergeTwoPatients(_patTo.PatNum,_patFrom.PatNum)) {
 				//The patient has been successfully merged.
 				//Now copy the physical images from the old patient to the new if they are using an AtoZ image share.
 				//This has to happen in the UI because the middle tier server might not have access to the image share.
 				//If the users are storing images within the database, those images have already been taken care of in the merge method above.
+				int fileCopyFailures=0;
 				if(PrefC.AtoZfolderUsed) {
 					#region Copy AtoZ Documents
 					//Move the patient documents within the 'patFrom' A to Z folder to the 'patTo' A to Z folder.
 					//We have to be careful here of documents with the same name. We have to rename such documents
 					//so that no documents are overwritten/lost.
 					string atoZpath=ImageStore.GetPreferredAtoZpath();
-					string atozFrom=ImageStore.GetPatientFolder(patientFrom,atoZpath);
-					string atozTo=ImageStore.GetPatientFolder(patientTo,atoZpath);
-					int fileCopyFailures=0;
+					string atozFrom=ImageStore.GetPatientFolder(_patFrom,atoZpath);
+					string atozTo=ImageStore.GetPatientFolder(_patTo,atoZpath);
 					string[] fromFiles=Directory.GetFiles(atozFrom);
-					for(int i=0;i<fromFiles.Length;i++) {
-						string fileName=Path.GetFileName(fromFiles[i]);
+					foreach(string fileCur in fromFiles) {
+						string fileName=Path.GetFileName(fileCur);
 						string destFileName=fileName;
 						string destFilePath=ODFileUtils.CombinePaths(atozTo,fileName);
 						if(File.Exists(destFilePath)) {
 							//The file being copied has the same name as a possibly different file within the destination a to z folder.
 							//We need to copy the file under a unique file name and then make sure to update the document table to reflect
 							//the change.
-							destFileName=patientFrom.PatNum.ToString()+"_"+fileName;
+							destFileName=_patFrom.PatNum.ToString()+"_"+fileName;
 							destFilePath=ODFileUtils.CombinePaths(atozTo,destFileName);
 							while(File.Exists(destFilePath)) {
-								destFileName=patientFrom.PatNum.ToString()+"_"+fileName+"_"+DateTime.Now.ToString("yyyyMMddhhmmss");
+								destFileName=_patFrom.PatNum.ToString()+"_"+fileName+"_"+DateTime.Now.ToString("yyyyMMddhhmmss");
 								destFilePath=ODFileUtils.CombinePaths(atozTo,destFileName);
 							}
 						}
-						bool isCopied=true;
 						try {
-							File.Copy(fromFiles[i],destFilePath); //Will throw exception if file already exists.
+							File.Copy(fileCur,destFilePath); //Will throw exception if file already exists.
 						}
-						catch {
-							isCopied=false;
+						catch(Exception ex) {
 							fileCopyFailures++;
+							continue;//copy failed, increment counter and move onto the next file
 						}
-						if(isCopied) {//If the copy did not fail, try to delete the old file.
-							//We can now safely update the document FileName and PatNum to the "to" patient.
-							Documents.MergePatientDocument(patFrom,patTo,fileName,destFileName);
-							try {
-								File.Delete(fromFiles[i]);
-							}
-							catch {
-								//If we were unable to delete the file then it is probably because someone has the document open currently.
-								//Just skip deleting the file. This means that occasionally there will be an extra file in their backup
-								//which is just clutter but at least the merge is guaranteed this way.
-							}
+						//If the copy did not fail, try to delete the old file.
+						//We can now safely update the document FileName and PatNum to the "to" patient.
+						Documents.MergePatientDocument(_patFrom.PatNum,_patTo.PatNum,fileName,destFileName);
+						try {
+							File.Delete(fileCur);
+						}
+						catch(Exception ex) {
+							//If we were unable to delete the file then it is probably because someone has the document open currently.
+							//Just skip deleting the file. This means that occasionally there will be an extra file in their backup
+							//which is just clutter but at least the merge is guaranteed this way.
 						}
 					}
-					if(fileCopyFailures>0) {
-						MessageBox.Show(Lan.g(this,"Some files belonging to the from patient were not copied.")+"\r\n"
-						                +Lan.g(this,"Number of files not copied")+": "+fileCopyFailures);
-					}
-					#endregion
+					#endregion Copy AtoZ Documents
+				}//end if AtoZFolderUsed
+				Cursor=Cursors.Default;
+				if(fileCopyFailures>0) {
+					MessageBox.Show(Lan.g(this,"Some files belonging to the from patient were not copied.")+"\r\n"
+						+Lan.g(this,"Number of files not copied")+": "+fileCopyFailures);
 				}
-				this.textPatientIDFrom.Text="";
-				this.textPatientNameFrom.Text="";
-				this.textPatFromBirthdate.Text="";
+				textPatientIDFrom.Text="";
+				textPatientNameFrom.Text="";
+				textPatFromBirthdate.Text="";
 				CheckUIState();
-				MsgBox.Show(this,"Patients merged successfully.");
 				//Make log entry here not in parent form because we can merge multiple patients at a time.
-				SecurityLogs.MakeLogEntry(Permissions.PatientMerge,patientTo.PatNum,"Patient: "+patientFrom.GetNameFL()+"\r\nPatNum From: "+patientFrom.PatNum+"\r\nPatNum To: "+patientTo.PatNum);
-			}
-			this.Cursor=Cursors.Default;
+				SecurityLogs.MakeLogEntry(Permissions.PatientMerge,_patTo.PatNum,
+					"Patient: "+_patFrom.GetNameFL()+"\r\nPatNum From: "+_patFrom.PatNum+"\r\nPatNum To: "+_patTo.PatNum);
+				MsgBox.Show(this,"Patients merged successfully.");
+			}//end MergeTwoPatients
+			Cursor=Cursors.Default;
 		}
 
 		private void butClose_Click(object sender,EventArgs e) {
