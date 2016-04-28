f32bb73a33e44092de416e822c7d5735d9c93662
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-04-28 23:14:21 +0000

    Update Files were failing to insert into the database when there are extra files located within the installation directory that caused unnecessary bloat. Backported to: 15.4.50.0;16.1.20.0 Reviewed by: Derek - This is phase II of the update files shenanigans. - We now store the Update Files in 1MB chunks within the documentmisc table so that the max_allowed_packet will stop happening. - Also, snuck in an Oracle fix due to missing ClinicNum in a programproperty insert statement in the convert script.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14269 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/DocumentMiscs.cs
+++ OpenDentBusiness/Data Interface/DocumentMiscs.cs
@@ -7,57 +7,34 @@ using System.Text;
 namespace OpenDentBusiness{
 	///<summary></summary>
 	public class DocumentMiscs{
-		///<summary>Can return null</summary>
-		public static DocumentMisc GetUpdateFilesZip() {
+
+		public static DocumentMisc GetByTypeAndFileName(string fileName,DocumentMiscType docMiscType) {
 			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				return Meth.GetObject<DocumentMisc>(MethodBase.GetCurrentMethod());
+				return Meth.GetObject<DocumentMisc>(MethodBase.GetCurrentMethod(),fileName,docMiscType);
 			}
-			//There will be either one or zero
-			string command="SELECT * FROM documentmisc WHERE DocMiscType="+POut.Long((int)DocumentMiscType.UpdateFiles);
+			string command="SELECT * FROM documentmisc "
+				+"WHERE DocMiscType="+POut.Int((int)docMiscType)+" "
+				+"AND FileName='"+POut.String(fileName)+"'";
 			return Crud.DocumentMiscCrud.SelectOne(command);
 		}
 
-		///<summary>Completely deletes the UpdateFiles row and then inserts a new one and returns the PK of the new record.</summary>
-		public static long SetUpdateFilesZip() {
+		///<summary></summary>
+		public static long Insert(DocumentMisc documentMisc) {
 			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				return Meth.GetLong(MethodBase.GetCurrentMethod());
+				documentMisc.DocMiscNum=Meth.GetLong(MethodBase.GetCurrentMethod(),documentMisc);
+				return documentMisc.DocMiscNum;
 			}
-			string command="DELETE FROM documentmisc WHERE DocMiscType="+POut.Long((int)DocumentMiscType.UpdateFiles);
+			return Crud.DocumentMiscCrud.Insert(documentMisc);
+		}
+
+		///<summary></summary>
+		public static void DeleteAllForType(DocumentMiscType docMiscType) {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),docMiscType);
+				return;
+			}
+			string command="DELETE FROM documentmisc WHERE DocMiscType="+POut.Int((int)docMiscType);
 			Db.NonQ(command);
-			DocumentMisc doc=new DocumentMisc();
-			doc.DateCreated=DateTime.Today;
-			doc.DocMiscType=DocumentMiscType.UpdateFiles;
-			doc.FileName="UpdateFiles.zip";
-			doc.RawBase64="";
-			return Crud.DocumentMiscCrud.Insert(doc);
-		}
-
-		///<summary>Appends the passed in rawBase64 string to the RawBase64 column in the db for the corresponding document.</summary>
-		public static void AppendRawBase64ForDoc(string rawBase64,long docMiscNum){
-			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				Meth.GetVoid(MethodBase.GetCurrentMethod(),rawBase64,docMiscNum);
-				return;
-			}
-			string command="UPDATE documentmisc SET RawBase64=CONCAT("+DbHelper.IfNull("RawBase64","")+","+DbHelper.ParamChar+"paramRawBase64) "
-				+"WHERE DocMiscNum="+POut.Long(docMiscNum);
-			OdSqlParameter paramRawBase64=new OdSqlParameter("paramRawBase64",OdDbType.Text,rawBase64);
-			Db.NonQ(command,paramRawBase64);
-		}
-
-		///<summary>Appends the passed in rawBase64 string to the RawBase64 column in the db for the Update Files document row.
-		///This method is used instead of AppendRawBase64ForDoc() because a large customer was having the following issue:
-		///The "Recopy" button was returning successfully but the RawBase64 column of the UpdateFiles row was actually empty.
-		///Jason and Derek came to the conclusion that this could only happen if the PK was incorrectly returned after inserting the UpdateFiles row.
-		///Therefore, this method will ignore the PK and instead will update all UpdateFiles rows (should only be one).</summary>
-		public static void AppendRawBase64ForUpdateFiles(string rawBase64) {
-			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				Meth.GetVoid(MethodBase.GetCurrentMethod(),rawBase64);
-				return;
-			}
-			string command="UPDATE documentmisc SET RawBase64=CONCAT("+DbHelper.IfNull("RawBase64","")+","+DbHelper.ParamChar+"paramRawBase64) "
-				+"WHERE DocMiscType="+POut.Int((int)DocumentMiscType.UpdateFiles);
-			OdSqlParameter paramRawBase64=new OdSqlParameter("paramRawBase64",OdDbType.Text,rawBase64);
-			Db.NonQ(command,paramRawBase64);
 		}
 
 		/*
