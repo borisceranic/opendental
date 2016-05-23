dd7796b309f72a914a2315de37b458dcfbe72878
chris <chris@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-05-23 18:23:46 +0000

    Moving an appointment would sometimes have a popup saying "Ask patient to arrive 0 minutes early". Reviewed by: Ryan Backported to: 16.1.23; 16.2.6
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental16.2@14405 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/ContrAppt.cs
+++ OpenDental/Main Modules/ContrAppt.cs
@@ -3580,7 +3580,9 @@ namespace OpenDental {
 							+" "+Lan.g(this,"minutes early at")+" "+apt.DateTimeAskedToArrive.ToShortTimeString()+".");
 					}
 					else {
-						if(apt.DateTimeAskedToArrive.Year>1880) {
+						if(apt.DateTimeAskedToArrive.Year>1880
+							&& (aptOld.AptDateTime-aptOld.DateTimeAskedToArrive).TotalMinutes>0) 
+						{
 							apt.DateTimeAskedToArrive=apt.AptDateTime-(aptOld.AptDateTime-aptOld.DateTimeAskedToArrive);
 							if(MessageBox.Show(Lan.g(this,"Ask patient to arrive")+" "+(aptOld.AptDateTime-aptOld.DateTimeAskedToArrive).TotalMinutes
 								+" "+Lan.g(this,"minutes early at")+" "+apt.DateTimeAskedToArrive.ToShortTimeString()+"?","",MessageBoxButtons.YesNo)==DialogResult.No) {
