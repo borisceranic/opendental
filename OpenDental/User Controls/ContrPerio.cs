39d437d1ec85bea81363bc129a8f02ed9286430d
saul <saul@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-05-27 21:16:50 +0000

    Perio Chart, new perio exam would sometimes cause OD to crash Backported to: 16.1.24.0;16.2.9.0 Reviewed by:Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14450 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/ContrPerio.cs
+++ OpenDental/User Controls/ContrPerio.cs
@@ -922,6 +922,7 @@ namespace OpenDental
 			}//for examI
 			//Start in the very first cell on the first tooth and loop through teeth until we come across one that is not missing.
 			CurCell=new Point(1,GetTableRow(selectedExam,0,PerioSequenceType.Probing));
+			OnDirectionChangedLeft();//Always start looping to the left.
 			int curTooth=GetToothNumCur(GetSection(CurCell.Y));
 			while(skippedTeeth.Contains(curTooth)) {
 				AdvanceCell();//Advance forward 3 times, since there are 3 measurements per tooth.
