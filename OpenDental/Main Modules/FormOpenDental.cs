03485ad6a8b4d6134d21524a8905ad5a60ccfa9c
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-05-31 18:29:34 +0000

    Appointment Bubble, would sometimes appear blank when a refresh occurred. Backported to: 16.1.25.0;16.2.10.0 Reviewed by: Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental16.2@14457 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/FormOpenDental.cs
+++ OpenDental/Main Modules/FormOpenDental.cs
@@ -4564,7 +4564,7 @@ namespace OpenDental{
 				}
 			}
 			if(ContrAppt2.Visible && Signalods.ApptNeedsRefresh(listSignals,AppointmentL.DateSelected.Date)) {
-				ContrAppt2.RefreshPeriod();
+				ContrAppt2.RefreshPeriod(false);
 			}
 			bool areAnySignalsTasks = false;
 			for(int i = 0;i<listSignals.Count;i++) {
