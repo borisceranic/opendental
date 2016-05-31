b7ce88193ea584844d7155ece19ffdf8c2c4772b
andrew <andrew@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-05-31 17:40:20 +0000

    HQ Only, MapAreaContainerNum is now part of the convert script. Reviewed by: Jason
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@14456 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/ConvertDatabases4.cs
+++ OpenDentBusiness/Misc/ConvertDatabases4.cs
@@ -1431,6 +1431,34 @@ namespace OpenDentBusiness {
 				command="UPDATE preference SET ValueString = '16.2.7.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
+			To16_2_10();
+		}
+
+		private static void To16_2_10() {
+			if(FromVersion<new Version("16.2.10.0")) {
+				ODEvent.Fire(new ODEventArgs("ConvertDatabases","Upgrading database to version: 16.2.10"));//No translation in convert script.
+				string command;
+				if(DataConnection.DBtype==DatabaseType.MySql) {
+					command="ALTER TABLE maparea ADD MapAreaContainerNum bigint NOT NULL";
+					Db.NonQ(command);
+					command="ALTER TABLE maparea ADD INDEX (MapAreaContainerNum)";
+					Db.NonQ(command);
+				}
+				else {//oracle
+					command="ALTER TABLE maparea ADD MapAreaContainerNum number(20)";
+					Db.NonQ(command);
+					command="UPDATE maparea SET MapAreaContainerNum = 0 WHERE MapAreaContainerNum IS NULL";
+					Db.NonQ(command);
+					command="ALTER TABLE maparea MODIFY MapAreaContainerNum NOT NULL";
+					Db.NonQ(command);
+					command=@"CREATE INDEX maparea_MapAreaContainerNum ON maparea (MapAreaContainerNum)";
+					Db.NonQ(command);
+				}
+				command="UPDATE maparea SET MapAreaContainerNum=1";
+				Db.NonQ(command);
+				command="UPDATE preference SET ValueString = '16.2.10.0' WHERE PrefName = 'DataBaseVersion'";
+				Db.NonQ(command);
+			}
 			To16_3_0();
 		}
 
