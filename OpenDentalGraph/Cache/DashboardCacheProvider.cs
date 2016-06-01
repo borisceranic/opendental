8d67f5c0a501a7923aaf85dcc996e669196e62a3
andrew <andrew@098d0599-7f2b-0410-95a5-ad30ac85393a>
2016-06-01 23:31:54 +0000

    Graphical Reports, when a provider's color was white or not set, their graphs were difficult to see. Backported to: 16.2.11.0 Reviewed by: Sam
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental16.2@14475 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentalGraph/Cache/DashboardCacheProvider.cs
+++ OpenDentalGraph/Cache/DashboardCacheProvider.cs
@@ -8,6 +8,8 @@ namespace OpenDentalGraph.Cache {
 	public class DashboardCacheProvider:DashboardCacheBase<Provider> {
 		private Dictionary<long,string> _dictProvNames=new Dictionary<long, string>();
 		private Dictionary<string,Color> _dictProvColors=new Dictionary<string, Color>();
+		private Random rand=new Random(0);
+
 		protected override List<Provider> GetCache(DashboardFilter filter) {
 			List<Provider> list=ProviderC.GetListLong();
 			_dictProvNames=list.ToDictionary(x => x.ProvNum,x => string.IsNullOrEmpty(x.Abbr)?x.ProvNum.ToString():x.Abbr);
@@ -29,8 +31,14 @@ namespace OpenDentalGraph.Cache {
 
 		public Color GetProvColor(string provAbbr) {
 			Color provColor;
-			if(!_dictProvColors.TryGetValue(provAbbr,out provColor)) {
-				return Color.DarkBlue;
+			if(!_dictProvColors.TryGetValue(provAbbr,out provColor) //if the provAbbr doesn't exist in the dict
+				|| provColor==Color.Empty //or the color was empty
+				|| provColor==Color.FromArgb(255,255,255) //or if the color was white
+				|| provColor==Color.FromArgb(240,240,240)) { //or if the user never specified a color for the prov
+				int r=(int)(rand.NextDouble()*255);
+				int g=(int)(rand.NextDouble()*255);
+				int b=(int)(rand.NextDouble()*255);
+				provColor=Color.FromArgb(r,g,b); //get a random color.
 			}
 			return provColor;
 		}
